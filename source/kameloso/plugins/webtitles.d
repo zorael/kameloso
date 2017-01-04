025b15ad236965183978763860bc3f1a60fcbcb2
JR <zorael@gmail.com>
2017-01-04 19:47:16 +0100

    Make Notes plugin use new common filtering code

--- source/kameloso/plugins/webtitles.d
+++ source/kameloso/plugins/webtitles.d
@@ -83,97 +83,41 @@ public:
         state.bot = bot;
     }
 
-    version(none)
     void onEvent(const IrcEvent event)
     {
-        return state.onEventImpl(event, &onCommand);
-    }
-
-    void onEvent(const IrcEvent event)
-    {
-        import std.algorithm.searching : canFind;
-
         with (state)
         with (IrcEvent.Type)
         switch (event.type)
         {
         case CHAN:
-            /*
-            * Not all channel messages are of interest; only those starting with the bot's
-            * nickname, those from whitelisted users, and those in channels marked as active.
-            */
-
-            if (!bot.channels.canFind(event.channel))
+            if (state.filterChannel!(RequirePrefix.no)(event) == FilterResult.fail)
             {
-                // Channel is not relevant
+                // Invalid channel
                 return;
             }
-
-            auto user = event.sender in users;
-
-            if (!user)
-            {
-                // No known user, relevant channel
-                return state.doWhois(event, &onCommand);
-            }
-
-            // User exists in users database
-            if (user.login == bot.master)
-            {
-                // User is master, all is ok
-                return onCommand(event);
-            }
-            else if (bot.friends.canFind(user.login))
-            {
-                // User is whitelisted, all is ok
-                return onCommand(event);
-            }
-            else
-            {
-                // Known bad user
-                return;
-            }
+            break;
 
         case QUERY:
-            // Queries are always aimed toward the bot, but the user must be whitelisted
-            auto user = event.sender in state.users;
-
-            if (!user) return state.doWhois(event, &onCommand);
-            else if ((user.login == bot.master) || bot.friends.canFind(user.login))
-            {
-                // master or friend
-                return onCommand(event);
-            }
-            break;
-
-        case WHOISLOGIN:
-            // Save user to users, then replay any queued commands.
-            state.users[event.target] = userFromEvent(event);
-            //users[event.target].lastWhois = Clock.currTime;
-
-            if (auto oldCommand = event.target in queue)
-            {
-                if ((*oldCommand)())
-                {
-                    // The command returned true; remove it from the queue
-                    queue.remove(event.target);
-                }
-            }
-
-            break;
-
-        case RPL_ENDOFWHOIS:
-            // If there's still a queued command at this point, WHOISLOGIN was never triggered
-            queue.remove(event.target);
-            break;
-
-        case PART:
-        case QUIT:
-            users.remove(event.sender);
+            // All queries are okay
             break;
 
         default:
-            break;
+            state.onBasicEvent(event);
+            return;
+        }
+
+        final switch (state.filterUser(event))
+        {
+        case FilterResult.pass:
+            // It is a known good user (friend or master), but it is of any type
+            return onCommand(event);
+
+        case FilterResult.whois:
+            return state.doWhois(event);
+
+        case FilterResult.fail:
+            // It is a known bad user
+            return;
         }
     }
 
