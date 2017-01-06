cf4409982477a28b278a0bd7e78eab5cd7334560
JR <zorael@gmail.com>
2017-01-06 21:37:52 +0100

    Add more work on the Notes plugin
    It now saves, loads and prints notes, but lacks the ability to delete
    sent ones, be it manually or automatically upon sending.

--- source/kameloso/plugins/notes.d
+++ source/kameloso/plugins/notes.d
@@ -3,8 +3,15 @@ module kameloso.plugins.notes;
 import kameloso.plugins.common;
 import kameloso.irc;
 import kameloso.common;
+import kameloso.constants;
 
 import std.json;
+import std.datetime;
+import std.conv;
+import std.string;
+import std.format;
+import std.stdio;
+
 
 private:
 
@@ -12,7 +19,6 @@ public:
 final class NotesPlugin : IrcPlugin
 {
 private:
-    import std.stdio : writeln, writefln;
     import std.concurrency : Tid, send;
 
     IrcPluginState state;
@@ -20,21 +26,95 @@ private:
 
     void onCommand(const IrcEvent event)
     {
+        import kameloso.stringutils;
+        import std.string : indexOf, stripLeft, munch;
+        import std.format : formattedRead;
+
         writeln("notes onCommand");
+
+        with(state)
         with (IrcEvent.Type)
         switch (event.type)
         {
         case JOIN:
-            writeln("Should look up notes on " ~ event.sender);
-            break;
+            writeln("Should look up notes on ", event.sender);
+            return onJoin(event);
 
         case CHAN:
-            //writeln("Line should be prefixed");
-            //writeln("Should react, verb addnote?");
-            break;
+            // Line should be prefixed here
+            string line;
+            string slice = event.content;
+            const hits = slice.formattedRead(bot.nickname ~ ":%s", &line);
+            if (!hits)
+            {
+                writeln("NO HITS?!");
+                writeln(line);
+                writeln(slice);
+                writeln(hits);
+                return;
+            }
+            line.munch(" :");
+            return onVerb(event, line);
 
         default:
-            writeln("default");
+            writeln("notes default");
+            break;
+        }
+    }
+
+    void onJoin(const IrcEvent event)
+    {
+        // Authorised and everything
+        foreach (note; notes.getNotes(event.sender))
+        {
+            const timestamp = "%s %02d/%02d %02d:%02d".format(note.when.dayOfWeek,
+                note.when.day, note.when.month, note.when.hour, note.when.minute);
+            state.mainThread.send(ThreadMessage.Sendline(),
+                "PRIVMSG %s :%s (%s): %s".format(event.channel, note.sender, timestamp, note.line));
+        }
+    }
+
+    void onVerb(const IrcEvent event, string line)
+    {
+        import kameloso.stringutils;
+        import std.uni : toLower;
+
+        const verb = line.nom!(Decode.yes)(' ');
+        writeln("VERB: ", verb);
+        writeln("LINE: ", line);
+
+        switch (verb.toLower)
+        {
+        case "addnote":
+            writeln("should add note");
+            string nickname, content;
+            auto hits = line.formattedRead("%s %s", &nickname, &content);
+            writeln("hits: ", hits);
+            notes.addNote(nickname, event.sender, content);
+            Files.notes.saveNotes(notes);
+            break;
+
+        case "getnotes":
+            const nickname = (line.indexOf(' ') == -1) ? line : line.nom(' ');
+            auto note = notes.getNotes(nickname);
+            writeln(note);
+            break;
+
+        case "printnotes":
+            writeln(notes.toPrettyString);
+            break;
+
+        case "fakejoin":
+            writeln("faking an event");
+            IrcEvent newEvent = event;
+            newEvent.sender = line;
+            newEvent.content = string.init;
+            newEvent.type = IrcEvent.Type.JOIN;
+            writeln(newEvent);
+            return onJoin(newEvent);
+
+        default:
+            writeln("notes default");
             break;
         }
     }
@@ -97,7 +177,143 @@ public:
         }
     }
 
-
     void teardown() {}
 }
 
+
+static auto getNotes(ref JSONValue notes, const string nickname)
+{
+    struct Note
+    {
+        string sender, line;
+        SysTime when;
+
+        version(none)
+        this(JSONValue note)
+        {
+            sender = note["sender"].str;
+            line = note["sline"].str;
+            when = SysTime.fromISOString(note["when"].str);
+        }
+
+        string toString() const
+        {
+            return `[NOTE] %s: "%s"`.format(sender, line);
+        }
+    }
+
+    Note[] noteArr;
+
+    try
+    {
+        if (auto arr = nickname in notes)
+        {
+            noteArr.length = arr.array.length;
+
+            foreach (i, note; arr.array)
+            {
+                writeln("---------------------------------");
+                writeln(note.toPrettyString);
+                writeln("---------------------------------");
+                noteArr[i].sender = note["sender"].str;
+                noteArr[i].line = note["line"].str;
+                noteArr[i].when = SysTime.fromISOString(note["when"].str);
+            }
+
+            return noteArr;
+        }
+        else
+        {
+            writeln("No notes available for nickname ", nickname);
+            return noteArr;
+        }
+    }
+    catch (JSONException e)
+    {
+        return noteArr;
+    }
+}
+
+
+static void addNote(ref JSONValue notes, const string nickname, const string sender, const string line)
+{
+    mixin(scopeguard(entry));
+    import std.format : format;
+
+    assert((nickname.length && line.length),
+        "%s was passed an empty nickname(%s) or line(%s)".format(__FUNCTION__, nickname, line));
+
+    auto lineAsAA =
+    [
+        "sender" : sender,
+        "when"   : Clock.currTime.toISOString,
+        "line"   : line
+    ];
+
+    writeln("lineAsAA: ", lineAsAA);
+
+    try
+    {
+        if (auto arr = nickname in notes)
+        {
+            writeln(*arr);
+            notes[nickname].array ~= JSONValue(lineAsAA);
+
+        }
+        else
+        {
+            // No notes for nickname
+            notes.object[nickname] = JSONValue([ lineAsAA ]);
+        }
+    }
+    catch (JSONException e)
+    {
+        writeln(e.msg);
+        // No notes at all
+        notes = JSONValue("{}");
+        return notes.addNote(nickname, sender, line);
+    }
+}
+
+
+static void saveNotes(const string filename, const JSONValue notes)
+{
+    import std.stdio : File;
+    import std.file  : exists, isFile, remove;
+
+    if (filename.exists && filename.isFile)
+    {
+        remove(filename); // Wise?
+    }
+
+    auto f = File(filename, "a");
+    scope (exit) f.close();
+
+    f.write(notes.toPrettyString);
+    f.writeln();
+}
+
+
+static void loadNotes(const string filename, ref JSONValue notes)
+{
+    import std.stdio  : writefln;
+    import std.file   : exists, isFile, readText;
+    import std.string : chomp;
+    writefln("Loading notes");
+
+    if (!filename.exists)
+    {
+        writefln("%s does not exist", filename);
+        notes = parseJSON("{}");
+        filename.saveNotes(notes);
+        return;
+    }
+    else if (!filename.isFile)
+    {
+        writefln("%s is not a file", filename);
+        return;
+    }
+
+    auto wholeFile = filename.readText.chomp;
+    notes = parseJSON(wholeFile);
+}
