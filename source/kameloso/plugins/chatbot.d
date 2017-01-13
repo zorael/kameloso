07d896ee4c07d54144aaa219792e14bba2f9d459
JR <zorael@gmail.com>
2017-01-13 16:21:57 +0100

    Module-level-ify the Chatbot plugin

--- source/kameloso/plugins/chatbot.d
+++ source/kameloso/plugins/chatbot.d
@@ -8,12 +8,177 @@ import kameloso.common;
 
 import std.stdio : writeln, writefln;
 import std.json : JSONValue, parseJSON, JSONException;
+import std.concurrency : send;
 
 
 private:
 
 IrcPluginState state;
 JSONValue quotes;
+
+
+// onCommand
+/++
+    +  React to a command from an IRC user. At this point it is known to be aimed toward
+    +  the bot, and we know the caller to be whitelisted or master.
+    +  It makes sense to split it up into different strings (verb and whatever), and then
+    +  do a string switch on them. It is, however, ugly.
+    +
+    +  Params:
+    +      event = A complete IrcEvent to react to.
+    +/
+void onCommand(const IrcEvent event)
+{
+    import std.string : munch, indexOf, stripLeft;
+    import std.uni : toLower;
+    import std.format : format;
+
+    with (state)
+    {
+        string slice;
+
+        if (event.type == IrcEvent.Type.QUERY)
+        {
+            slice = event.content.stripLeft;
+        }
+        else
+        {
+            // We know it to be aimed at us from earlier checks so remove nickname prefix
+            slice = event.content.stripLeft[(bot.nickname.length+1)..$];
+            slice.munch(":?! ");
+        }
+
+        string verb;
+
+        // If we don't decode here the verb will be truncated if it contiains international characters
+        if (slice.indexOf(' ') != -1)
+        {
+            verb = slice.nom!(Decode.yes)(' ');
+        }
+        else
+        {
+            verb = slice;
+            slice = string.init;
+        }
+
+        const target = (event.channel.length) ? event.channel : event.sender;
+
+        switch(verb.toLower)
+        {
+        case "8ball":
+            import std.random : uniform;
+
+            // Get a random 8ball message and send it
+            const reply = eightballAnswers[uniform(0, eightballAnswers.length)];
+            mainThread.send(ThreadMessage.Sendline(),
+                "PRIVMSG %s :%s".format(target, reply));
+            break;
+
+        case "säg":
+        case "say":
+            // Simply repeat what was said
+            mainThread.send(ThreadMessage.Sendline(),
+                "PRIVMSG %s :%s".format(target, slice));
+            break;
+
+        case "quote":
+            // Get a quote from the JSON list and send it
+            if (!slice.length) break;
+
+            // stripModeSign to allow for quotes from @nickname and +dudebro
+            const nickname = slice.stripModeSign;
+            const quote = nickname.getQuote();
+
+            if (quote.length)
+            {
+                mainThread.send(ThreadMessage.Sendline(),
+                    "PRIVMSG %s :%s | %s".format(target, nickname, quote));
+            }
+            else
+            {
+                mainThread.send(ThreadMessage.Sendline(),
+                    "PRIVMSG %s :No quote on record for %s".format(target, nickname));
+            }
+            break;
+
+        case "addquote":
+            // Add a quote to the JSON list and save it to disk
+            const nickname = slice.nom!(Decode.yes)(' ').stripModeSign;
+
+            if (!nickname.length || !slice.length) return;
+
+            nickname.addQuote(slice);
+            Files.quotes.saveQuotes(quotes);
+
+            mainThread.send(ThreadMessage.Sendline(),
+                "PRIVMSG %s :Quote for %s saved (%d on record)"
+                .format(target, nickname, quotes[nickname].array.length));
+            break;
+
+        case "printquotes":
+            // Print all quotes to the terminal
+            writeln(quotes.toPrettyString);
+            break;
+
+        case "reloadquotes":
+            // Reload quotes from disk (in case they were manually changed)
+            writeln("Reloading quotes");
+            Files.quotes.loadQuotes(quotes);
+            break;
+
+        default:
+            // writefln("Chatbot unknown verb:%s", verb);
+            // do nothing
+            break;
+        }
+    }
+}
+
+
+/++
+    +  React to an IrcEvent from the server. At this point nothing about it is known, and only
+    +  events from whitelisted users (and master) should be reacted to. So; logic to ensure
+    +  the user is authorised, and that the message was aimed toward the bot.
+    +
+    +  Params:
+    +      event = A complete IrcEvent to react to.
+    +/
+void onEvent(const IrcEvent event)
+{
+    with (state)
+    with (IrcEvent.Type)
+    switch (event.type)
+    {
+    case CHAN:
+        if (state.filterChannel!(RequirePrefix.yes)(event) == FilterResult.fail)
+        {
+            // Invalid channel or not prefixed
+            return;
+        }
+        break;
+
+    case QUERY:
+        break;
+
+    default:
+        state.onBasicEvent(event);
+        return;
+    }
+
+    final switch (state.filterUser(event))
+    {
+    case FilterResult.pass:
+        // It is a known good user (friend or master), but it is of any type
+        return onCommand(event);
+
+    case FilterResult.whois:
+        return state.doWhois(event);
+
+    case FilterResult.fail:
+        // It is a known bad user
+        return;
+    }
+}
 
 
 public:
@@ -25,133 +190,17 @@ public:
  +/
 final class Chatbot : IrcPlugin
 {
-private:
-    import std.concurrency : Tid, send;
-    import std.algorithm : canFind;
-
-    // onCommand
-    /++
-     +  React to a command from an IRC user. At this point it is known to be aimed toward
-     +  the bot, and we know the caller to be whitelisted or master.
-     +  It makes sense to split it up into different strings (verb and whatever), and then
-     +  do a string switch on them. It is, however, ugly.
-     +
-     +  Params:
-     +      event = A complete IrcEvent to react to.
-     +/
-    void onCommand(const IrcEvent event)
-    {
-        import std.string : munch, indexOf, stripLeft;
-        import std.uni : toLower;
-        import std.format : format;
-
-        with (state)
-        {
-            string slice;
-
-            if (event.type == IrcEvent.Type.QUERY)
-            {
-                slice = event.content.stripLeft;
-            }
-            else
-            {
-                // We know it to be aimed at us from earlier checks so remove nickname prefix
-                slice = event.content.stripLeft[(bot.nickname.length+1)..$];
-                slice.munch(":?! ");
-            }
-
-            string verb;
-
-            // If we don't decode here the verb will be truncated if it contiains international characters
-            if (slice.indexOf(' ') != -1)
-            {
-                verb = slice.nom!(Decode.yes)(' ');
-            }
-            else
-            {
-                verb = slice;
-                slice = string.init;
-            }
-
-            const target = (event.channel.length) ? event.channel : event.sender;
-
-            switch(verb.toLower)
-            {
-            case "8ball":
-                import std.random : uniform;
-
-                // Get a random 8ball message and send it
-                const reply = eightballAnswers[uniform(0, eightballAnswers.length)];
-                mainThread.send(ThreadMessage.Sendline(),
-                    "PRIVMSG %s :%s".format(target, reply));
-                break;
-
-            case "säg":
-            case "say":
-                // Simply repeat what was said
-                mainThread.send(ThreadMessage.Sendline(),
-                    "PRIVMSG %s :%s".format(target, slice));
-                break;
-
-            case "quote":
-                // Get a quote from the JSON list and send it
-                if (!slice.length) break;
-
-                // stripModeSign to allow for quotes from @nickname and +dudebro
-                const nickname = slice.stripModeSign;
-                const quote = nickname.getQuote();
-
-                if (quote.length)
-                {
-                    mainThread.send(ThreadMessage.Sendline(),
-                        "PRIVMSG %s :%s | %s".format(target, nickname, quote));
-                }
-                else
-                {
-                    mainThread.send(ThreadMessage.Sendline(),
-                        "PRIVMSG %s :No quote on record for %s".format(target, nickname));
-                }
-                break;
-
-            case "addquote":
-                // Add a quote to the JSON list and save it to disk
-                const nickname = slice.nom!(Decode.yes)(' ').stripModeSign;
-
-                if (!nickname.length || !slice.length) return;
-
-                nickname.addQuote(slice);
-                Files.quotes.saveQuotes(quotes);
-
-                mainThread.send(ThreadMessage.Sendline(),
-                    "PRIVMSG %s :Quote for %s saved (%d on record)"
-                    .format(target, nickname, quotes[nickname].array.length));
-                break;
-
-            case "printquotes":
-                // Print all quotes to the terminal
-                writeln(quotes.toPrettyString);
-                break;
-
-            case "reloadquotes":
-                // Reload quotes from disk (in case they were manually changed)
-                writeln("Reloading quotes");
-                Files.quotes.loadQuotes(quotes);
-                break;
-
-            default:
-                // writefln("Chatbot unknown verb:%s", verb);
-                // do nothing
-                break;
-            }
-        }
-    }
-
 public:
     this(IrcPluginState origState)
     {
         state = origState;
 
         Files.quotes.loadQuotes(quotes);
+    }
+
+    void onEvent(const IrcEvent event)
+    {
+        return event.onEvent();
     }
 
     void status()
@@ -163,51 +212,6 @@ public:
     void newBot(IrcBot bot)
     {
         state.bot = bot;
-    }
-
-    /++
-     +  React to an IrcEvent from the server. At this point nothing about it is known, and only
-     +  events from whitelisted users (and master) should be reacted to. So; logic to ensure
-     +  the user is authorised, and that the message was aimed toward the bot.
-     +
-     +  Params:
-     +      event = A complete IrcEvent to react to.
-     +/
-    void onEvent(const IrcEvent event)
-    {
-        with (state)
-        with (IrcEvent.Type)
-        switch (event.type)
-        {
-        case CHAN:
-            if (state.filterChannel!(RequirePrefix.yes)(event) == FilterResult.fail)
-            {
-                // Invalid channel or not prefixed
-                return;
-            }
-            break;
-
-        case QUERY:
-            break;
-
-        default:
-            state.onBasicEvent(event);
-            return;
-        }
-
-        final switch (state.filterUser(event))
-        {
-        case FilterResult.pass:
-            // It is a known good user (friend or master), but it is of any type
-            return onCommand(event);
-
-        case FilterResult.whois:
-            return state.doWhois(event);
-
-        case FilterResult.fail:
-            // It is a known bad user
-            return;
-        }
     }
 
     /// No teardown neccessary for Chatbot
