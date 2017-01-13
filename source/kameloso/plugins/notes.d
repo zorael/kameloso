297cc56f62d4ed55f92cbfde73bab89bd3de0f98
JR <zorael@gmail.com>
2017-01-13 16:32:05 +0100

    Module-level-ify the Notes plugin

--- source/kameloso/plugins/notes.d
+++ source/kameloso/plugins/notes.d
@@ -15,166 +15,144 @@ private:
 IrcPluginState state;
 JSONValue notes;
 
-public:
 
-
-final class NotesPlugin : IrcPlugin
+void onCommand(const IrcEvent event)
 {
-private:
-    import std.concurrency : Tid, send;
+    import kameloso.stringutils;
+    import std.string : indexOf, munch;
 
-    void onCommand(const IrcEvent event)
+    with(state)
+    with (IrcEvent.Type)
+    switch (event.type)
     {
-        import kameloso.stringutils;
-        import std.string : indexOf, munch;
+    case JOIN:
+        return onJoin(event);
 
-        with(state)
-        with (IrcEvent.Type)
-        switch (event.type)
+    case CHAN:
+        // Line should be prefixed here
+        string line;
+        string slice = event.content;
+        const hits = slice.formattedRead(bot.nickname ~ ":%s", &line);
+        if (!hits)
         {
-        case JOIN:
-            return onJoin(event);
-
-        case CHAN:
-            // Line should be prefixed here
-            string line;
-            string slice = event.content;
-            const hits = slice.formattedRead(bot.nickname ~ ":%s", &line);
-            if (!hits)
-            {
-                writeln("NO HITS?!");
-                writeln(line);
-                writeln(slice);
-                writeln(hits);
-                return;
-            }
-            line.munch(" :");
-            return onVerb(event, line);
-
-        default:
-            break;
-        }
-    }
-
-    void onJoin(const IrcEvent event)
-    {
-        // Authorised and everything
-        auto noteArray = getNotes(event.sender);
-        if (!noteArray.length) return;
-
-        foreach (note; noteArray)
-        {
-            const timestamp = "%s %02d/%02d %02d:%02d".format(note.when.dayOfWeek,
-                note.when.day, note.when.month, note.when.hour, note.when.minute);
-            state.mainThread.send(ThreadMessage.Sendline(),
-                "PRIVMSG %s :%s! %s left note on %s: %s"
-                .format(event.channel, event.sender, note.sender, timestamp, note.line));
-        }
-
-        clearNotes(event.sender);
-    }
-
-    void onVerb(const IrcEvent event, string line)
-    {
-        import kameloso.stringutils;
-        import std.string : indexOf;
-        import std.uni : toLower;
-
-        string verb, args;
-        line.formattedRead("%s %s", &verb, &args);
-
-        switch (verb.toLower)
-        {
-        case "addnote":
-        case "note":
-            string nickname, content;
-            const hits = args.formattedRead("%s %s", &nickname, &content);
-            if (hits != 2) return;
-            addNote(nickname, event.sender, content);
-            state.mainThread.send(ThreadMessage.Sendline(),
-                "PRIVMSG %s :Note added".format(event.channel));
-            Files.notes.saveNotes(notes);
-            break;
-
-        case "printnotes":
-            if (event.sender != state.bot.master) return;
-            writeln(notes.toPrettyString);
-            break;
-
-        case "fakejoin":
-            if (event.sender != state.bot.master) return;
-            writeln("faking an event");
-            IrcEvent newEvent = event;
-            newEvent.sender = args;
-            newEvent.content = string.init;
-            newEvent.type = IrcEvent.Type.JOIN;
-            writeln(newEvent);
-            return onJoin(newEvent);
-
-        default:
-            writeln("notes default");
-            break;
-        }
-    }
-
-public:
-    this(IrcPluginState origState)
-    {
-        state = origState;
-
-        Files.notes.loadNotes(notes);
-    }
-
-    void status()
-    {
-        writeln("---------------------- ", typeof(this).stringof);
-        printObject(state);
-    }
-
-    void newBot(IrcBot bot)
-    {
-        state.bot = bot;
-    }
-
-    void onEvent(const IrcEvent event)
-    {
-        with (state)
-        with (IrcEvent.Type)
-        switch (event.type)
-        {
-        case CHAN:
-            if (state.filterChannel!(RequirePrefix.yes)(event) == FilterResult.fail)
-            {
-                // Invalid channel or not prefixed
-                return;
-            }
-            break;
-
-        case QUERY:
-        case JOIN:
-            break;
-
-        default:
-            state.onBasicEvent(event);
+            writeln("NO HITS?!");
+            writeln(line);
+            writeln(slice);
+            writeln(hits);
             return;
         }
+        line.munch(" :");
+        return onVerb(event, line);
 
-        final switch (state.filterUser(event))
-        {
-        case FilterResult.pass:
-            // It is a known good user (friend or master), but it is of any type
-            return onCommand(event);
+    default:
+        break;
+    }
+}
 
-        case FilterResult.whois:
-            return state.doWhois(event);
 
-        case FilterResult.fail:
-            // It is a known bad user
-            return;
-        }
+void onJoin(const IrcEvent event)
+{
+    import std.concurrency : send;
+
+    // Authorised and everything
+    auto noteArray = getNotes(event.sender);
+    if (!noteArray.length) return;
+
+    foreach (note; noteArray)
+    {
+        const timestamp = "%s %02d/%02d %02d:%02d".format(note.when.dayOfWeek,
+            note.when.day, note.when.month, note.when.hour, note.when.minute);
+        state.mainThread.send(ThreadMessage.Sendline(),
+            "PRIVMSG %s :%s! %s left note on %s: %s"
+            .format(event.channel, event.sender, note.sender, timestamp, note.line));
     }
 
-    void teardown() {}
+    clearNotes(event.sender);
+}
+
+
+void onVerb(const IrcEvent event, string line)
+{
+    import kameloso.stringutils;
+
+    import std.concurrency : send;
+    import std.string : indexOf;
+    import std.uni : toLower;
+
+    string verb, args;
+    line.formattedRead("%s %s", &verb, &args);
+
+    switch (verb.toLower)
+    {
+    case "addnote":
+    case "note":
+        string nickname, content;
+        const hits = args.formattedRead("%s %s", &nickname, &content);
+        if (hits != 2) return;
+        addNote(nickname, event.sender, content);
+        state.mainThread.send(ThreadMessage.Sendline(),
+            "PRIVMSG %s :Note added".format(event.channel));
+        Files.notes.saveNotes(notes);
+        break;
+
+    case "printnotes":
+        if (event.sender != state.bot.master) return;
+        writeln(notes.toPrettyString);
+        break;
+
+    case "fakejoin":
+        if (event.sender != state.bot.master) return;
+        writeln("faking an event");
+        IrcEvent newEvent = event;
+        newEvent.sender = args;
+        newEvent.content = string.init;
+        newEvent.type = IrcEvent.Type.JOIN;
+        writeln(newEvent);
+        return onJoin(newEvent);
+
+    default:
+        writeln("notes default");
+        break;
+    }
+}
+
+
+void onEvent(const IrcEvent event)
+{
+    with (state)
+    with (IrcEvent.Type)
+    switch (event.type)
+    {
+    case CHAN:
+        if (state.filterChannel!(RequirePrefix.yes)(event) == FilterResult.fail)
+        {
+            // Invalid channel or not prefixed
+            return;
+        }
+        break;
+
+    case QUERY:
+    case JOIN:
+        break;
+
+    default:
+        state.onBasicEvent(event);
+        return;
+    }
+
+    final switch (state.filterUser(event))
+    {
+    case FilterResult.pass:
+        // It is a known good user (friend or master), but it is of any type
+        return onCommand(event);
+
+    case FilterResult.whois:
+        return state.doWhois(event);
+
+    case FilterResult.fail:
+        // It is a known bad user
+        return;
+    }
 }
 
 
@@ -306,4 +284,36 @@ static void loadNotes(const string filename, ref JSONValue notes)
 
     auto wholeFile = filename.readText.chomp;
     notes = parseJSON(wholeFile);
+}
+
+
+public:
+
+final class NotesPlugin : IrcPlugin
+{
+public:
+    this(IrcPluginState origState)
+    {
+        state = origState;
+
+        Files.notes.loadNotes(notes);
+    }
+
+    void onEvent(const IrcEvent event)
+    {
+        return event.onEvent();
+    }
+
+    void status()
+    {
+        writeln("---------------------- ", typeof(this).stringof);
+        printObject(state);
+    }
+
+    void newBot(IrcBot bot)
+    {
+        state.bot = bot;
+    }
+
+    void teardown() {}
 }
