0160c8fcac4c31d227f4ce32c64674240499eb21
JR <zorael@gmail.com>
2017-05-30 23:17:01 +0200

    Extend isValidChannel to handle &channels
    They're in the specs. Also unit test those.

--- source/kameloso/irc.d
+++ source/kameloso/irc.d
@@ -1564,14 +1564,36 @@ bool isValidChannel(const string line)
 {
     import std.string : indexOf;
 
-    return ((line.length > 1) && (line.indexOf(' ') == -1) && (line[0] == '#'));
+    /++
+     +  Channels names are strings (beginning with a '&' or '#' character) of
+     +  length up to 200 characters.  Apart from the the requirement that the
+     +  first character being either '&' or '#'; the only restriction on a
+     +  channel name is that it may not contain any spaces (' '), a control G
+     +  (^G or ASCII 7), or a comma (',' which is used as a list item
+     +  separator by the protocol).
+     +
+     +  https://tools.ietf.org/html/rfc1459.html
+     +/
+
+    if ((line.indexOf(' ') != -1) ||
+        (line.indexOf(',') != -1) ||
+        (line.indexOf(7) != -1))
+    {
+        return false;
+    }
+
+    if ((line.length <= 1) || (line.length > 200)) return false;
+
+    return ((line[0] == '#') || (line[0] == '&'));
 }
 unittest
 {
     assert("#channelName".isValidChannel);
+    assert("&otherChannel".isValidChannel);
     assert(!"#not a channel".isValidChannel);
     assert(!"notAChannelEither".isValidChannel);
     assert(!"#".isValidChannel);
+    assert(!"".isValidChannel);
 }
 
 
