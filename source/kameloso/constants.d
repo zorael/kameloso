9728c0ff632a21ecc509cef27c6bb5dff7317342
JR <zorael@gmail.com>
2017-06-01 14:40:41 +0200

    Bump version

--- source/kameloso/constants.d
+++ source/kameloso/constants.d
@@ -1,6 +1,6 @@
 module kameloso.constants;
 
-enum kamelosoVersion = "0.4.5";
+enum kamelosoVersion = "0.4.6";
 enum kamelosoSource = "https://github.com/zorael/kameloso";
 
 enum IRCControlCharacter
