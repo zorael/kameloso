a287c73348e461c281f930fe7f399c30c21db857
JR <zorael@gmail.com>
2017-11-12 19:12:50 +0100

    Remove commented code

--- source/kameloso/irc.d
+++ source/kameloso/irc.d
@@ -529,7 +529,6 @@ void parseSpecialcases(ref IRCEvent event, ref IRCBot bot, ref string slice)
             // :kameloso!~NaN@2001:41d0:2:80b4:: JOIN #hirrsteff2 kameloso : kameloso!
             event.channel = slice.nom(' ');
             event.sender.login = slice.nom(" :");
-            //if (event.sender.login == "*") event.sender.login = string.init;
             //event.content = slice.strip();  // no need for full name...
         }
         else
