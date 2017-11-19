bd2a386b3d42f65a70fce759437dd0046aa71eea
JR <zorael@gmail.com>
2017-11-19 12:02:08 +0100

    Add admin.d verb: asserts
    This will generate copy/pastable assert statement blocks for an easier
    way to create unittests.

--- source/kameloso/plugins/admin.d
+++ source/kameloso/plugins/admin.d
@@ -20,6 +20,243 @@ bool printAll;
 /// Toggles whether onAnyEvent prints the raw bytes of the *contents* of events
 bool printBytes;
 
+/// Toggles whether onAnyEvent prints assert statements for incoming events
+bool printAsserts;
+
+
+// tabs
+/++
+ +  Returns spaces equal to that of num tabs (\t).
+ +/
+string tabs(uint num) pure
+{
+    enum tab = "    ";
+
+    string total;
+
+    foreach (i; 0..num)
+    {
+        total ~= tab;
+    }
+
+    return total;
+}
+
+
+// formatAssertStatementLines
+/++
+ +  Constructs assert statement lines for each changed field of a type.
+ +/
+void formatAssertStatementLines(Sink, Thing)(auto ref Sink sink, Thing thing,
+    const string prefix = string.init, uint depth = 0)
+{
+    foreach (immutable i, value; thing.tupleof)
+    {
+        alias T = typeof(value);
+        enum memberstring = __traits(identifier, thing.tupleof[i]);
+
+        static if ((memberstring == "raw") || (memberstring == "time"))
+        {
+            continue;
+        }
+        else static if (is(T == struct))
+        {
+            sink.formatAssertStatementLines(thing.tupleof[i], memberstring, depth);
+        }
+        else
+        {
+            if (value != Thing.init.tupleof[i])
+            {
+                import std.format : formattedWrite;
+                import std.traits : isSomeString;
+
+                static if (isSomeString!T)
+                {
+                    enum pattern = "%sassert((%s%s == \"%s\"), %s%s);\n";
+                }
+                else
+                {
+                    enum pattern = "%sassert((%s%s == %s), %s%s.to!string);\n";
+                }
+
+                sink.formattedWrite(pattern,
+                    depth.tabs,
+                    prefix.length ? prefix ~ '.' : string.init,
+                    memberstring, value,
+                    prefix.length ? prefix ~ '.' : string.init,
+                    memberstring);
+            }
+        }
+    }
+}
+
+unittest
+{
+    import std.array : Appender;
+    Appender!string sink;
+    sink.reserve(512);
+
+    IRCBot bot;
+
+    immutable event = ":zorael!~NaN@2001:41d0:2:80b4:: PRIVMSG #flerrp :kameloso: 8ball"
+                      .toIRCEvent(bot);
+
+    sink.formatAssertStatementLines(event, string.init, 2);
+
+    assert(sink.data ==
+`        assert((type == CHAN), type.to!string);
+        assert((sender.nickname == "zorael"), sender.nickname);
+        assert((sender.ident == "~NaN"), sender.ident);
+        assert((sender.address == "2001:41d0:2:80b4::"), sender.address);
+        assert((channel == "#flerrp"), channel);
+        assert((content == "kameloso: 8ball"), content);
+`, '\n' ~ sink.data);
+}
+
+
+// formatAssertStatementLines
+/++
+ +  Constructs statement lines for each changed field of an IRCBot, including
+ +  instantiating a fresh one.
+ +/
+void formatBot(Sink)(auto ref Sink sink, const IRCBot bot)
+{
+
+    sink.put("IRCBot bot;\n");
+    sink.put("with (bot)\n");
+    sink.put("{\n");
+
+    foreach (immutable i, value; bot.tupleof)
+    {
+        import std.format : formattedWrite;
+        import std.traits : isSomeString;
+        alias T = typeof(value);
+        enum memberstring = __traits(identifier, bot.tupleof[i]);
+
+        static if (is(T == struct) || is(T == class))
+        {
+            // Can't recurse for now, future improvement
+            continue;
+        }
+        else
+        {
+            if (value != IRCBot.init.tupleof[i])
+            {
+                static if (isSomeString!T)
+                {
+                    enum pattern = "%s%s = \"%s\";\n";
+                }
+                else
+                {
+                    enum pattern = "%s%s = %s;\n";
+                }
+
+                sink.formattedWrite(pattern, 1.tabs, memberstring, value);
+            }
+        }
+
+    }
+
+    sink.put("}");
+
+    static if (!__traits(hasMember, Sink, "data"))
+    {
+        sink.put('\n');
+    }
+}
+
+unittest
+{
+    import std.array : Appender;
+
+    Appender!string sink;
+    sink.reserve(128);
+
+    IRCBot bot;
+    with (bot)
+    {
+        nickname = "NICKNAME";
+        user = "UUUUUSER";
+    }
+
+    sink.formatBot(bot);
+
+    assert(sink.data ==
+`IRCBot bot;
+with (bot)
+{
+    nickname = "NICKNAME";
+    user = "UUUUUSER";
+}`, '\n' ~ sink.data);
+}
+
+
+// formatEventAssertBlock
+/++
+ +  Constructs assert statement blocks for each changed field of an IRCEvent.
+ +/
+void formatEventAssertBlock(Sink)(auto ref Sink sink, const IRCEvent event)
+{
+    import std.array : Appender;
+    import std.format : formattedWrite;
+
+    sink.put("{\n");
+    sink.formattedWrite("%simmutable event = \"%s\"\n",
+        1.tabs, event.raw);
+    sink.formattedWrite("%s                  .toIRCEvent(bot);\n", 1.tabs);
+    sink.formattedWrite("%swith (event)\n", 1.tabs);
+    sink.formattedWrite("%s{\n", 1.tabs);
+    sink.formatAssertStatementLines(event, string.init, 2);
+    sink.formattedWrite("%s}\n", 1.tabs);
+    sink.put("}");
+
+    static if (!__traits(hasMember, Sink, "data"))
+    {
+        sink.put('\n');
+    }
+}
+
+unittest
+{
+    import std.array : Appender;
+    import std.format : formattedWrite;
+
+    Appender!string sink;
+    sink.reserve(512);
+
+    IRCBot bot;
+
+    immutable event = ":zorael!~NaN@2001:41d0:2:80b4:: PRIVMSG #flerrp :kameloso: 8ball"
+                      .toIRCEvent(bot);
+
+    // copy/paste the above
+    sink.put("{\n");
+    sink.formattedWrite("%simmutable event = \"%s\"\n",
+        1.tabs, event.raw);
+    sink.formattedWrite("%s                  .toIRCEvent(bot);\n", 1.tabs);
+    sink.formattedWrite("%swith (event)\n", 1.tabs);
+    sink.formattedWrite("%s{\n", 1.tabs);
+    sink.formatAssertStatementLines(event, string.init, 2);
+    sink.formattedWrite("%s}\n", 1.tabs);
+    sink.put("}");
+
+    assert(sink.data ==
+`{
+    immutable event = ":zorael!~NaN@2001:41d0:2:80b4:: PRIVMSG #flerrp :kameloso: 8ball"
+                      .toIRCEvent(bot);
+    with (event)
+    {
+        assert((type == CHAN), type.to!string);
+        assert((sender.nickname == "zorael"), sender.nickname);
+        assert((sender.ident == "~NaN"), sender.ident);
+        assert((sender.address == "2001:41d0:2:80b4::"), sender.address);
+        assert((channel == "#flerrp"), channel);
+        assert((content == "kameloso: 8ball"), content);
+    }
+}`, '\n' ~ sink.data);
+}
+
+
 
 // onCommandShowUsers
 /++
@@ -315,6 +552,24 @@ void onCommandPrintBytes()
 }
 
 
+// onCommandAsserts
+/++
+ +  Toggles a flag to print assert statements for incoming events.
+ +
+ +  This is for debugging purposes.
+ +/
+@(IRCEvent.Type.CHAN)
+@(IRCEvent.Type.QUERY)
+@(PrivilegeLevel.master)
+@Prefix(NickPolicy.required, "asserts")
+void onCommandAsserts()
+{
+    printAsserts = !printAsserts;
+    logger.info("Printing asserts: ", printAsserts);
+    formatBot(stdout.lockingTextWriter, state.bot);
+}
+
+
 // onAnyEvent
 /++
  +  Prints all incoming events raw if the flag to do so has been set with
@@ -337,6 +592,18 @@ void onAnyEvent(const IRCEvent event)
         {
             writefln("[%d] %s : %03d", i, cast(char)c, c);
         }
+    }
+
+    if (printAsserts)
+    {
+        import std.algorithm.searching : canFind;
+
+        if ((cast(ubyte[])event.raw).canFind(1))
+        {
+            logger.warning("event.raw contains CTCP 1 which might not get printed");
+        }
+
+        formatEventAssertBlock(stdout.lockingTextWriter, event);
     }
 }
 
