03002077574cea6b570b98cf58bcba35af5cfd62
JR <zorael@gmail.com>
2017-11-22 00:31:16 +0100

    Eleventh reduction

--- source/kameloso/constants.d
+++ source/kameloso/constants.d
@@ -1,57 +1,5 @@
 module kameloso.constants;
 
-/// Meta information about the program.
-enum KamelosoInfo
-{
-    version_ = "0.9.0",
-    built = __TIMESTAMP__,
-    source = "https://github.com/zorael/kameloso",
-}
-
-/// Certain characters that signal specific meaning in an IRC context.
-enum IRCControlCharacter
-{
-    ctcp = 1,
-    bold = 2,
-    colour = 3,
-    italics = 29,
-    underlined = 31,
-}
-
-/// When a nickname is taken, append this to get a new name.
-enum altNickSign = '^';
-
-/// Buffer sizes in bytes.
-enum BufferSize
-{
-    socketOptionReceive = 2048,
-    socketOptionSend = 1024,
-    socketReceive = 2048,
-    titleLookup = 8192,
-}
-
-/// Various timeouts in seconds.
-enum Timeout
-{
-    retry = 5,
-    send = 5,
-    receive = 1,
-    keepalive = 300,
-    resolve = 10,
-    ping = 200,
-    whois = 15,
-    titleCache = 600,
-}
-
-/// Bitflags used in combination with the scopeguard function, to generate scopeguard mixins.
-enum : ubyte
-{
-    entry   = 1 << 0,
-    exit    = 1 << 1,
-    success = 1 << 2,
-    failure = 1 << 3,
-}
-
 /// Special terminal control characters
 enum TerminalToken
 {
