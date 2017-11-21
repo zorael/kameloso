6e4794614d8ffdf9712f55c7feae0003f8b8ac47
JR <zorael@gmail.com>
2017-11-22 00:02:52 +0100

    Ninth reduction

--- source/kameloso/irc.d
+++ source/kameloso/irc.d
@@ -14,129 +14,6 @@ import std.stdio;
 
 private:
 
-/// Max nickname length as per IRC specs, but not the de facto standard
-uint maxNickLength = 9;
-
-/// Max channel name length as per IRC specs
-uint maxChannelLength = 200;
-
-
-// parseBasic
-/++
- +  Parses the most basic of IRC events; PING, ERROR, PONG and NOTICE.
- +
- +  They syntactically differ from other events in that they are not prefixed
- +  by their sender.
- +
- +  The IRCEvent is finished at the end of this function.
- +
- +  Params:
- +      ref event = the IRCEvent to fill out the members of.
- +/
-void parseBasic(ref IRCEvent event, ref IRCBot bot) @trusted
-{
-}
-
-// parsePrefix
-/++
- +  Takes a slice of a raw IRC string and starts parsing it into an IRCEvent struct.
- +
- +  This function only focuses on the prefix; the sender, be it nickname and ident
- +  or server address.
- +
- +  The IRCEvent is not finished at the end of this function.
- +
- +  Params:
- +      ref event = A reference to the IRCEvent to start working on.
- +      ref slice = A reference to the slice of the raw IRC string.
- +/
-void parsePrefix(ref IRCEvent event, ref IRCBot bot, ref string slice)
-{
-}
-
-// parseTypestring
-/++
- +  Takes a slice of a raw IRC string and continues parsing it into an IRCEvent struct.
- +
- +  This function only focuses on the typestring; the part that tells what kind of event
- +  happened, like PRIVMSG or MODE or NICK or KICK, etc; in string format.
- +
- +  The IRCEvent is not finished at the end of this function.
- +
- +  Params:
- +      ref event = A reference to the IRCEvent to continue working on.
- +      ref slice = A reference to the slice of the raw IRC string.
- +/
-void parseTypestring(ref IRCEvent event, ref IRCBot bot, ref string slice)
-{
-}
-
-// parseSpecialcases
-/++
- +  Takes a slice of a raw IRC string and continues parsing it into an IRCEvent struct.
- +
- +  This function only focuses on specialcasing the remaining line, dividing it
- +  into fields like target, channel, content, etc.
- +
- +  IRC events are *riddled* with inconsistencies, so this function is very very
- +  long but by neccessity.
- +
- +  The IRCEvent is finished at the end of this function.
- +
- +  Params:
- +      ref event = A reference to the IRCEvent to finish working on.
- +      ref slice = A reference to the slice of the raw IRC string.
- +/
-void parseSpecialcases(ref IRCEvent event, ref IRCBot bot, ref string slice)
-{
-}
-
-
-// postparseSanityCheck
-/++
- +  Checks for some specific erroneous edge cases in an IRCEvent, complains
- +  about all of them and corrects some.
- +
- +  Params:
- +      ref event = the IRC event to examine.
- +/
-void postparseSanityCheck(ref IRCEvent event, const IRCBot bot)
-{
-}
-
-
-// parseTwitchTags
-/++
- +  Parses a Twitch event's IRCv3 tags.
- +
- +  The event is passed by ref as many tags neccessitate changes to it.
- +
- +  Params:
- +      ref event = A reference to the IRCEvent whose tags should be parsed.
- +/
-void parseTwitchTags(ref IRCEvent event, ref IRCBot bot)
-{
-}
-
-
-// prioritiseTwoRoles
-/++
- +  Compares a given IRCEvent.Role to a role string and decides which of the
- +  two weighs the most; which takes precedence over the other.
- +
- +  This is used to decide what role a user has when they are of several at the
- +  same time. A moderator might be a partner and a subscriber at the same
- +  time, for instance.
- +
- +  Params:
- +      current = The right-hand-side IRCEvent.Role to compare with.
- +      newRole = A Role in lowercase, left-hand-side to compare with.
- +
- +  Returns:
- +      the IRCEvent.Role with the highest priority of the two.
- +/
-
-
 void onPRIVMSG(ref IRCEvent event, ref IRCBot bot, ref string slice)
 {
     import kameloso.stringutils : beginsWith;
@@ -168,7 +45,6 @@ void onPRIVMSG(ref IRCEvent event, ref IRCBot bot, ref string slice)
     }
 
     default:
-        logger.warning("-------------------- UNKNOWN CTCP EVENT");
         printObject(event);
         break;
     }
@@ -176,29 +52,8 @@ void onPRIVMSG(ref IRCEvent event, ref IRCBot bot, ref string slice)
 
 public:
 
-// toIRCEvent
-/++
- +  Parser an IRC string into an IRCEvent.
- +
- +  It passes it to the different parsing functions to get a finished IRCEvent.
- +  Parsing goes through several phases (prefix, typestring, specialcases) and
- +  this is the function that calls them.
- +
- +  Params:
- +      raw = The raw IRC string to parse.
- +
- +  Returns:
- +      A finished IRCEvent.
- +/
 IRCEvent toIRCEvent(const string raw, ref IRCBot bot)
 {
     return IRCEvent.init;
 }
 
-
-/// This simply looks at an event and decides whether it is from a nickname
-/// registration service.
-bool isFromAuthService(const IRCEvent event, ref IRCBot bot)
-{
-    return false;
-}
