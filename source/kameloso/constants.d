5660556c048ae8d7e8f2d5d7b4ae15ef57ced495
JR <zorael@gmail.com>
2017-11-24 11:39:14 +0100

    Bump version

--- source/kameloso/constants.d
+++ source/kameloso/constants.d
@@ -3,7 +3,7 @@ module kameloso.constants;
 /// Meta information about the program.
 enum KamelosoInfo
 {
-    version_ = "0.9.0",
+    version_ = "0.9.1",
     built = __TIMESTAMP__,
     source = "https://github.com/zorael/kameloso",
 }
