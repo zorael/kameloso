b9501fc07072956e5ca581452012306c057df700
JR <zorael@gmail.com>
2017-12-03 14:04:13 +0100

    stringutils.d this time, last was common.d

--- source/kameloso/stringutils.d
+++ source/kameloso/stringutils.d
@@ -67,118 +67,11 @@ string nom(Flag!"decode" decode = No.decode, T, C)(ref T[] arr, const C separato
     return arr[0..index];
 }
 
-unittest
-{
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom(" :");
-        assert(lorem == "Lorem ipsum", lorem);
-        assert(line == "sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom!(Yes.decode)(" :");
-        assert(lorem == "Lorem ipsum", lorem);
-        assert(line == "sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom(':');
-        assert(lorem == "Lorem ipsum ", lorem);
-        assert(line == "sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom!(Yes.decode)(':');
-        assert(lorem == "Lorem ipsum ", lorem);
-        assert(line == "sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom(' ');
-        assert(lorem == "Lorem", lorem);
-        assert(line == "ipsum :sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom!(Yes.decode)(' ');
-        assert(lorem == "Lorem", lorem);
-        assert(line == "ipsum :sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom("");
-        assert(!lorem.length, lorem);
-        assert(line == "Lorem ipsum :sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom!(Yes.decode)("");
-        assert(!lorem.length, lorem);
-        assert(line == "Lorem ipsum :sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom("Lorem ipsum");
-        assert(!lorem.length, lorem);
-        assert(line == " :sit amet", line);
-    }
-
-    {
-        string line = "Lorem ipsum :sit amet";
-        immutable lorem = line.nom!(Yes.decode)("Lorem ipsum");
-        assert(!lorem.length, lorem);
-        assert(line == " :sit amet", line);
-    }
-}
-
-
-// plurality
-/++
- +  Get the correct singular or plural form of a word depending on the
- +  numerical count of it.
- +
- +  Params:
- +      num = The numerical count of the noun.
- +      singular = The noun in singular form.
- +      plural = The noun in plural form.
- +
- +  Returns:
- +      The singular string if num is 1 or -1, otherwise the plural string.
- +/
 pragma(inline)
 string plurality(const int num, const string singular, const string plural) pure
 {
     return ((num == 1) || (num == -1)) ? singular : plural;
 }
-
-unittest
-{
-    assert(10.plurality("one","many") == "many");
-    assert(1.plurality("one", "many") == "one");
-    assert((-1).plurality("one", "many") == "one");
-    assert(0.plurality("one", "many") == "many");
-}
-
-
-// unquoted
-/++
- +  Removes one preceding and one trailing quote, unquoting a word.
- +
- +  Params:
- +      line = the (potentially) quoted string.
- +
- +  Returns:
- +      A slice of the line argument that excludes the quotes.
- +/
 pragma(inline)
 string unquoted(const string line) pure
 {
@@ -196,27 +89,6 @@ string unquoted(const string line) pure
     }
 }
 
-unittest
-{
-    assert(`"Lorem ipsum sit amet"`.unquoted == "Lorem ipsum sit amet");
-    assert(`"""""Lorem ipsum sit amet"""""`.unquoted == "Lorem ipsum sit amet");
-    // Unbalanced quotes are left untouched
-    assert(`"Lorem ipsum sit amet`.unquoted == `"Lorem ipsum sit amet`);
-}
-
-
-// beginsWith
-/++
- +  A cheaper variant of std.algorithm.searching.startsWith, since it is
- +  such a hotspot.
- +
- +  Params:
- +      haystack = The original line to examine.
- +      needle = The snippet of text to check if haystack begins with.
- +
- +  Returns:
- +      true if haystack starts with needle, otherwise false.
- +/
 pragma(inline)
 bool beginsWith(T)(const T haystack, const T needle) pure
 if (isSomeString!T)
@@ -229,15 +101,6 @@ if (isSomeString!T)
     return (haystack[0..needle.length] == needle);
 }
 
-unittest
-{
-    assert("Lorem ipsum sit amet".beginsWith("Lorem ip"));
-    assert(!"Lorem ipsum sit amet".beginsWith("ipsum sit amet"));
-    assert("Lorem ipsum sit amet".beginsWith(""));
-}
-
-
-/// Ditto
 pragma(inline)
 bool beginsWith(T)(const T line, const ubyte charcode) pure
 if (isSomeString!T)
@@ -246,13 +109,6 @@ if (isSomeString!T)
 
     return (line[0] == charcode);
 }
-
-unittest
-{
-    assert(":Lorem ipsum".beginsWith(':'));
-    assert(!":Lorem ipsum".beginsWith(';'));
-}
-
 
 // arrayify
 /++
@@ -276,15 +132,6 @@ T[] arrayify(string separator = ",", T)(const T line)
     return line.splitter(separator).map!(a => a.strip).array;
 }
 
-unittest
-{
-    assert("foo,bar,baz".arrayify     == [ "foo", "bar", "baz" ]);
-    assert("foo|bar|baz".arrayify!"|" == [ "foo", "bar", "baz" ]);
-    assert("foo bar baz".arrayify!" " == [ "foo", "bar", "baz" ]);
-    assert("only one entry".arrayify  == [ "only one entry" ]);
-    assert("not one entry".arrayify!" "  == [ "not", "one", "entry" ]);
-    assert("".arrayify == []);
-}
 
 
 /// stripPrefix
@@ -314,25 +161,6 @@ string stripPrefix(const string line, const string prefix)
     auto hits = slice.matchFirst(engine);
     return hits[1];
 }
-
-unittest
-{
-    immutable lorem = "say: lorem ipsum".stripPrefix("say");
-    assert((lorem == "lorem ipsum"), lorem);
-
-    immutable notehello = "note!!!! zorael hello".stripPrefix("note");
-    assert((notehello == "zorael hello"), notehello);
-
-    immutable sudoquit = "sudo quit :derp".stripPrefix("sudo");
-    assert((sudoquit == "quit :derp"), sudoquit);
-
-    immutable eightball = "8ball predicate?".stripPrefix("");
-    assert((eightball == "8ball predicate?"), eightball);
-
-    immutable isabot = "kamelosois a bot".stripPrefix("kameloso");
-    assert((isabot == "is a bot"), isabot);
-}
-
 
 // timeSince
 /++
@@ -387,40 +215,6 @@ string timeSince(const Duration duration)
     sink.reserve(50);
     sink.timeSince(duration);
     return sink.data;
-}
-
-unittest
-{
-    immutable dur1 = 789_383.seconds;  // 1 week, 2 days, 3 hours, 16 minutes, and 23 secs
-    assert((dur1.timeSince == "9 days, 3 hours and 16 minutes"), dur1.timeSince);
-
-    immutable dur2 = 3_620.seconds;  // 1 hour and 20 secs
-    assert((dur2.timeSince == "1 hour"), dur2.timeSince);
-
-    immutable dur3 = 30.seconds;  // 30 secs
-    assert((dur3.timeSince == "30 seconds"), dur3.timeSince);
-
-    immutable dur4 = 1.seconds;
-    assert((dur4.timeSince == "1 second"), dur4.timeSince);
-
-    import std.array : Appender;
-
-    Appender!(char[]) sink;
-    sink.reserve(64);  // workaround for LDC
-
-    immutable dur5 = 0.seconds;
-    sink.timeSince(dur5);
-    assert((sink.data == "0 seconds"), sink.data);
-    sink.clear();
-
-    immutable dur6 = 3_141_519_265.msecs;
-    sink.timeSince(dur6);
-    assert((sink.data == "36 days, 8 hours and 38 minutes"), sink.data);
-    sink.clear();
-
-    immutable dur7 = 3599.seconds;
-    sink.timeSince(dur7);
-    assert((sink.data == "59 minutes"), sink.data);
 }
 
 
@@ -538,19 +332,6 @@ if (is(Enum == enum))
     }
 }
 
-unittest
-{
-    import kameloso.irc : IRCEvent;
-
-    with (IRCEvent)
-    with (IRCEvent.Type)
-    {
-        assert(enumToString(QUERY) == "QUERY");
-        assert(enumToString(PRIVMSG) == "PRIVMSG");
-        assert(enumToString(RPL_ENDOFMOTD) == "RPL_ENDOFMOTD");
-    }
-}
-
 
 // numFromHex
 /++
@@ -607,20 +388,6 @@ int numFromHex(Flag!"acceptLowercase" acceptLowercase = No.acceptLowercase)
 }
 
 
-// numFromHex
-/++
- +  Convenience wrapper that takes a hex string and maps the values to three
- +  integers passed by ref.
- +
- +  This is to be used when mapping a #RRGGBB colour to their decimal
- +  red/green/blue equivalents.
- +
- +  Params:
- +      hexString = a string with a hexadecimal number (colour)
- +      ref r = ref int for the red part of the hex string
- +      ref g = ref int for the green part of the hex string
- +      ref b = ref int for the blue part of the hex string
- +/
 void numFromHex(Flag!"acceptLowercase" acceptLowercase = No.acceptLowercase)
     (const string hexString, out int r, out int g, out int b)
 {
@@ -632,47 +399,6 @@ void numFromHex(Flag!"acceptLowercase" acceptLowercase = No.acceptLowercase)
     g = numFromHex!acceptLowercase(hex[2..4]);
     b = numFromHex!acceptLowercase(hex[4..$]);
 }
-
-unittest
-{
-    import std.conv : text;
-    {
-        int r, g, b;
-        numFromHex("000102", r, g, b);
-
-        assert((r == 0), r.text);
-        assert((g == 1), g.text);
-        assert((b == 2), b.text);
-    }
-
-    {
-        int r, g, b;
-        numFromHex("FFFFFF", r, g, b);
-
-        assert((r == 255), r.text);
-        assert((g == 255), g.text);
-        assert((b == 255), b.text);
-    }
-
-    {
-        int r, g, b;
-        numFromHex("3C507D", r, g, b);
-
-        assert((r == 60), r.text);
-        assert((g == 80), g.text);
-        assert((b == 125), b.text);
-    }
-
-    {
-        int r, g, b;
-        numFromHex!(Yes.acceptLowercase)("9a4B7c", r, g, b);
-
-        assert((r == 154), r.text);
-        assert((g == 75), g.text);
-        assert((b == 124), b.text);
-    }
-}
-
 
 string stripSuffix(Flag!"allowFullStrip" fullStrip = No.allowFullStrip)
     (const string line, const string suffix) pure
@@ -688,14 +414,4 @@ string stripSuffix(Flag!"allowFullStrip" fullStrip = No.allowFullStrip)
 
     return (line[($-suffix.length)..$] == suffix) ?
         line[0..($-suffix.length)] : line;
-}
-
-unittest
-{
-    immutable line = "harblsnarbl";
-    assert(line.stripSuffix("snarbl") == "harbl");
-    assert(line.stripSuffix("") == "harblsnarbl");
-    assert(line.stripSuffix("INVALID") == "harblsnarbl");
-    assert(!line.stripSuffix!(Yes.allowFullStrip)("harblsnarbl").length);
-    assert(line.stripSuffix("harblsnarbl") == "harblsnarbl");
 }
