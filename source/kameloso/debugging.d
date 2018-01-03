937b560b5900d6a1ccd7705fe3f79690a87568d7
JR <zorael@gmail.com>
2018-01-03 15:27:12 +0100

    Skip target.special when generating assert blocks
    It's so far not in use, so it's always false.

--- source/kameloso/debugging.d
+++ source/kameloso/debugging.d
@@ -31,6 +31,11 @@ void formatAssertStatementLines(Sink, Thing)(auto ref Sink sink, Thing thing,
         alias T = Unqual!(typeof(value));
         enum memberstring = __traits(identifier, thing.tupleof[i]);
 
+        // IRCEvent.target.special is at present never true
+        if ((prefix == "target") && (memberstring == "special")) continue;
+
+        // IRCEvent.raw is always visible in the parser.toIRCEvent command
+        // and the time timestamp is not something we take into consideration
         static if ((memberstring == "raw") || (memberstring == "time"))
         {
             continue;
