8a842677b8417e994c16caab1efa04ff0a339226
JR <zorael@gmail.com>
2018-01-07 14:01:56 +0100

    Add {logger,traits,uda}.d
    Effort to split up common.d, which is becoming conflated.

---
+++ source/kameloso/traits.d
@@ -0,0 +1,196 @@
+module kameloso.traits;
+
+import kameloso.uda;
+
+import std.traits : Unqual, isType;
+
+// isConfigurableVariable
+/++
+ +  Eponymous template bool of whether a variable can be configured via the
+ +  functions in `kameloso.config` or not.
+ +
+ +  Currently it does not support static arrays.
+ +
+ +  Params:
+ +      var = alias of variable to examine.
+ +/
+template isConfigurableVariable(alias var)
+{
+    static if (!isType!var)
+    {
+        import std.traits : isSomeFunction;
+
+        alias T = typeof(var);
+
+        enum isConfigurableVariable =
+            !isSomeFunction!T &&
+            !__traits(isTemplate, T) &&
+            !__traits(isAssociativeArray, T) &&
+            !__traits(isStaticArray, T);
+    }
+    else
+    {
+        enum isConfigurableVariable = false;
+    }
+}
+
+///
+unittest
+{
+    int i;
+    char[] c;
+    char[8] c2;
+    struct S {}
+    class C {}
+    enum E { foo }
+    E e;
+
+    static assert(isConfigurableVariable!i);
+    static assert(isConfigurableVariable!c);
+    static assert(!isConfigurableVariable!c2); // should static arrays pass?
+    static assert(!isConfigurableVariable!S);
+    static assert(!isConfigurableVariable!C);
+    static assert(!isConfigurableVariable!E);
+    static assert(isConfigurableVariable!e);
+}
+
+
+// longestMemberName
+/++
+ +  Gets the name of the longest member in one or more struct/class objects.
+ +
+ +  This is used for formatting configuration files, so that columns line up.
+ +
+ +  Params:
+ +      Things = the types to examine and count name lengths
+ +/
+template longestMemberName(Things...)
+if (Things.length > 0)
+{
+    enum longestMemberName = ()
+    {
+        import std.traits : hasUDA;
+
+        string longest;
+
+        foreach (T; Things)
+        {
+            foreach (name; __traits(allMembers, T))
+            {
+                static if (!isType!(__traits(getMember, T, name)) &&
+                    isConfigurableVariable!(__traits(getMember, T, name)) &&
+                    !hasUDA!(__traits(getMember, T, name), Hidden) &&
+                    !hasUDA!(__traits(getMember, T, name), Unconfigurable))
+                {
+                    if (name.length > longest.length)
+                    {
+                        longest = name;
+                    }
+                }
+            }
+        }
+
+        return longest;
+    }();
+}
+
+///
+unittest
+{
+    struct Foo
+    {
+        string veryLongName;
+        int i;
+        @Unconfigurable string veryVeryVeryLongNameThatIsInvalid;
+        @Hidden float likewiseWayLongerButInvalid;
+    }
+
+    struct Bar
+    {
+        string evenLongerName;
+        float f;
+
+        @Unconfigurable
+        @Hidden
+        long looooooooooooooooooooooong;
+    }
+
+    static assert(longestMemberName!Foo == "veryLongName");
+    static assert(longestMemberName!Bar == "evenLongerName");
+    static assert(longestMemberName!(Foo, Bar) == "evenLongerName");
+}
+
+
+// isOfAssignableType
+/++
+ +  Eponymous template bool of whether a variable is "assignable"; if it is
+ +  an lvalue that isn't protected from being written to.
+ +/
+template isOfAssignableType(T)
+if (isType!T)
+{
+    import std.traits : isSomeFunction;
+
+    enum isOfAssignableType = isType!T &&
+        !isSomeFunction!T &&
+        !is(T == const) &&
+        !is(T == immutable);
+}
+
+
+/// Ditto
+enum isOfAssignableType(alias symbol) = isType!symbol && is(symbol == enum);
+
+///
+unittest
+{
+    struct Foo
+    {
+        string bar, baz;
+    }
+
+    class Bar
+    {
+        int i;
+    }
+
+    void boo(int i) {}
+
+    enum Baz { abc, def, ghi }
+    Baz baz;
+
+    assert(isOfAssignableType!int);
+    assert(!isOfAssignableType!(const int));
+    assert(!isOfAssignableType!(immutable int));
+    assert(isOfAssignableType!(string[]));
+    assert(isOfAssignableType!Foo);
+    assert(isOfAssignableType!Bar);
+    assert(!isOfAssignableType!boo);  // room for improvement: @property
+    assert(isOfAssignableType!Baz);
+    assert(!isOfAssignableType!baz);
+    assert(isOfAssignableType!string);
+}
+
+
+// UnqualArray
+/++
+ +  Given an array of qualified elements, aliases itself to one such of
+ +  unqualified elements.
+ +/
+alias UnqualArray(QualArray : QualType[], QualType) = Unqual!QualType[];
+
+///
+unittest
+{
+    alias ConstStrings = const(string)[];
+    alias UnqualStrings = UnqualArray!ConstStrings;
+    static assert(is(UnqualStrings == string[]));
+
+    alias ImmChars = string;
+    alias UnqualChars = UnqualArray!ImmChars;
+    static assert(is(UnqualChars == char[]));
+
+    alias InoutBools = inout(bool)[];
+    alias UnqualBools = UnqualArray!InoutBools;
+    static assert(is(UnqualBools == bool[]));
+}
---
+++ source/kameloso/uda.d
@@ -0,0 +1,19 @@
+module kameloso.uda;
+
+
+/// UDA conveying that a field is not to be saved in configuration files
+struct Unconfigurable {}
+
+
+/// UDA conveying that a string is an array with this token as separator
+struct Separator
+{
+    /// Separator, can be more than one character
+    string token = ",";
+}
+
+/++
+ +  UDA conveying that this member contains sensitive information and should not
+ + be printed in clear text; e.g. passwords
+ +/
+struct Hidden {}
