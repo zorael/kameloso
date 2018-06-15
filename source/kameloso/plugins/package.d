ff17bc9a44498fdbaa1e22dcf0cbc02458349bc5
JR <zorael@gmail.com>
2018-06-15 16:58:45 +0200

    Initial dustmite reduction

--- source/kameloso/plugins/package.d
+++ source/kameloso/plugins/package.d
@@ -1,52 +1,8 @@
-/++
- +  This `package` file contains the list of `EnabledPlugins`, to which you
- +  append your plugin to have it be instantiated and included in the bot's
- +  normal routines.
- +/
+
 module kameloso.plugins;
 
-import std.meta : AliasSeq;
-
-/++
- +  Publically import all plugins so that only `kameloso.plugins` need be
- +  imported.
- +/
 public import kameloso.plugins.common;
-/*public import kameloso.plugins.admin;
-public import kameloso.plugins.chatbot;
-public import kameloso.plugins.connect;
-public import kameloso.plugins.ctcp;
-public import kameloso.plugins.notes;
-public import kameloso.plugins.printer;
-public import kameloso.plugins.sedreplace;
-public import kameloso.plugins.twitch;
-public import kameloso.plugins.seen;
-public import kameloso.plugins.chanqueries;
-public import kameloso.plugins.persistence;
-public import kameloso.plugins.automode;
-public import kameloso.plugins.quotes;*/
 
 
-/++
- +  List of enabled plugins. Add and remove to enable and disable.
- +
- +  Note that `dub` will still compile any files in the `plugins` directory!
- +  To completely omit a plugin you will either have to compile the bot
- +  manually, or add an `__EOF__` at the top of the plugin source file.
- +/
-public alias EnabledPlugins = AliasSeq!();
-/*    TwitchService, // Must be before PersistenceService
-    PersistenceService, // Should be early
-    PrinterPlugin,  // Might as well be early
-    ConnectService,
-    ChanQueriesService,
-    CTCPService,
-    AdminPlugin,
-    ChatbotPlugin,
-    NotesPlugin,
-    SedReplacePlugin,
-    SeenPlugin,
-    AutomodePlugin,
-    QuotesPlugin,
-);
-*/
+
+
