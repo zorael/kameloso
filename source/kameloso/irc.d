84a6b27bc587d1d3efa31999686bf7a7ac127d11
JR <zorael@gmail.com>
2018-06-23 02:39:35 +0200

    Add kameloso.irc.containsNickname
    Examines a string and searches for a supplied nickname.
    
    It looks at characters before and after the substring nickname to ensure
    that it's not just the wanted nick in the middle of a different one. In
    other words, "kameloso" should not contain "kam".

--- source/kameloso/irc.d
+++ source/kameloso/irc.d
@@ -2692,7 +2692,64 @@ unittest
         }
     }
 
-    assert(!(' '.isValidNicknameCharacter));
+    assert(!' '.isValidNicknameCharacter);
+}
+
+
+// containsNickname
+/++
+ +  Searches a string for a substring that isn't surrounded by characters that
+ +  can be part of a nickname. This can detect a nickname in a string without
+ +  getting false positives from similar nicknames.
+ +
+ +  Uses `std.string.indexOf` internally with hopes of being more resilient to
+ +  weird UTF-8.
+ +
+ +  Params:
+ +      haystack = A string to search for the substring nickname.
+ +      needle = The nickname substring to find in `haystack`.
+ +
+ +  Returns:
+ +      True if `haystack` contains `needle` in such a way that it is guaranteed
+ +      to not be a different nickname.
+ +/
+bool containsNickname(const string haystack, const string needle) pure
+{
+    import std.string : indexOf, representation;
+
+    if ((haystack.length == needle.length) && (haystack == needle)) return true;
+
+    // What do we do with empty strings?
+    if (!needle.length) return false;
+
+    immutable pos = haystack.indexOf(needle);
+    if (pos == -1) return false;
+
+    if ((pos > 0) && (haystack[pos-1].isValidNicknameCharacter)) return false;
+
+    immutable end = pos + needle.length;
+
+    if (end > haystack.length)
+    {
+        return false;
+    }
+    else if (end == haystack.length)
+    {
+        return true;
+    }
+
+    return (!haystack[end].isValidNicknameCharacter);
+}
+
+///
+unittest
+{
+    assert("kameloso".containsNickname("kameloso"));
+    assert(" kameloso ".containsNickname("kameloso"));
+    assert(!"kam".containsNickname("kameloso"));
+    assert(!"kameloso^".containsNickname("kameloso"));
+    assert(!string.init.containsNickname("kameloso"));
+    assert(!"kameloso".containsNickname(""));  // For now let this be false.
 }
 
 
