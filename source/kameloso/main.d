d0fbca3b4047d1704d9ebb6f974a6b69d5346ead
JR <zorael@gmail.com>
2018-09-15 01:31:41 +0200

    More feedback about address

--- source/kameloso/main.d
+++ source/kameloso/main.d
@@ -4,11 +4,15 @@ void main(string[] args)
 {
     import kameloso.connection : ResolveAttempt, resolveFiber;
     import std.concurrency;
+    import std.stdio;
 
     Client client;
     client.parser.bot.server.address = (args.length > 1) ? args[1] : "wefpok";
+    writeln("Attempting to resolve ", client.parser.bot.server.address);
 
     new Generator!ResolveAttempt(() => resolveFiber(client.conn,
         client.parser.bot.server.address, client.parser.bot.server.port,
         settings.ipv6, *client.abort));
+
+    writeln("Success");
 }
