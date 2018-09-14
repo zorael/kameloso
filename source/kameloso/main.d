23bd0fb08f06dbc4d734576e4a8c15137c2ac80a
JR <zorael@gmail.com>
2018-09-15 01:29:52 +0200

    Allow for overriding address with args

--- source/kameloso/main.d
+++ source/kameloso/main.d
@@ -1,12 +1,12 @@
 import kameloso.common;
 
-void main()
+void main(string[] args)
 {
     import kameloso.connection : ResolveAttempt, resolveFiber;
     import std.concurrency;
 
     Client client;
-    client.parser.bot.server.address = "wefpok";
+    client.parser.bot.server.address = (args.length > 1) ? args[1] : "wefpok";
 
     new Generator!ResolveAttempt(() => resolveFiber(client.conn,
         client.parser.bot.server.address, client.parser.bot.server.port,
