ac246c69b27f99188dd2966b3132237d849ee969
JR <zorael@gmail.com>
2018-09-18 13:05:02 +0200

    Improve kameloso.plugins.package
    By use of a string mixin, only import plugins whose source files are
    actually present. This makes it easier to disable plugins you don't want
    enabled. Mind that you need to physically remove the files from the
    source tree to not have them be compiled by dub.

--- source/kameloso/plugins/package.d
+++ source/kameloso/plugins/package.d
@@ -1,61 +1,123 @@
 /++
- +  This `package` file contains the list of `EnabledPlugins`, to which you
- +  append your plugin to have it be instantiated and included in the bot's
- +  normal routines.
+ +  This `package` file contains the lists of enabled plugins (`EnabledPlugins`,
+ +  `EnabledWebPlugins` and `EnabledPosixPlugins`) to which you append your
+ +  plugin to have it be instantiated and included in the bot's normal routines.
  +/
 module kameloso.plugins;
 
-import std.meta : AliasSeq;
-
-/++
- +  Publically import all plugins so that only `kameloso.plugins` need be
- +  imported.
- +/
-public import kameloso.plugins.admin;
-public import kameloso.plugins.chatbot;
 public import kameloso.plugins.common;
-public import kameloso.plugins.connect;
-public import kameloso.plugins.ctcp;
-public import kameloso.plugins.notes;
-public import kameloso.plugins.printer;
-public import kameloso.plugins.sedreplace;
-public import kameloso.plugins.seen;
-public import kameloso.plugins.chanqueries;
-public import kameloso.plugins.persistence;
-public import kameloso.plugins.automode;
-public import kameloso.plugins.quotes;
 
-version(Posix)
+
+// tryImportMixin
+/++
+ +  String mixin. If a module is available, import it. If it isn't available,
+ +  alias the passed `alias_` to an empty `AliasSeq`.
+ +
+ +  This allows us to import modules if they exist but otherwise silently still
+ +  let it work without them.
+ +
+ +  Params:
+ +      module_ = Fully qualified string name of the module to evaluate and
+ +          potentially import.
+ +      alias_ = Name of the symbol to create that points to an empty `AliasSeq`
+ +          iff the module was not imported.
+ +
+ +  Returns:
+ +      A selectively-importing `static if`. Mix this in to use.
+ +/
+version(WithPlugins)
+string tryImportMixin(const string module_, const string alias_)
 {
-    // Implicitly enabled if imported.
-    public import kameloso.plugins.pipeline;
+    import std.format;
+
+    return q{
+    static if (__traits(compiles, %1$s))
+        {
+            pragma(msg, "Importing plugin: %1$s");
+            public import %1$s;
+        }
+        else
+        {
+            import std.meta : AliasSeq;
+            alias %2$s = AliasSeq!();
+        }
+    }.format(module_, alias_);
 }
 
-version(Web)
+
+version(WithPlugins)
 {
-    // Implicitly enabled if imported.
-    public import kameloso.plugins.webtitles;
-    public import kameloso.plugins.bashquotes;
-    public import kameloso.plugins.reddit;
+    /+
+     +  Selectively import the plugins that are available. If not, alias the symbol
+     +  with the name of the second parameter to an empty AliasSeq.
+     +/
+    mixin(tryImportMixin("kameloso.plugins.admin", "AdminPlugin"));
+    mixin(tryImportMixin("kameloso.plugins.chatbot", "ChatbotPlugin"));
+    mixin(tryImportMixin("kameloso.plugins.connect", "ConnectService"));
+    mixin(tryImportMixin("kameloso.plugins.ctcp", "CTCPService"));
+    mixin(tryImportMixin("kameloso.plugins.notes", "NotesPlugin"));
+    mixin(tryImportMixin("kameloso.plugins.printer", "PrinterPlugin"));
+    mixin(tryImportMixin("kameloso.plugins.sedreplace", "SedReplacePlugin"));
+    mixin(tryImportMixin("kameloso.plugins.seen", "SeenPlugin"));
+    mixin(tryImportMixin("kameloso.plugins.chanqueries", "ChanQueriesService"));
+    mixin(tryImportMixin("kameloso.plugins.persistence", "PersistenceService"));
+    mixin(tryImportMixin("kameloso.plugins.automode", "AutomodePlugin"));
+    mixin(tryImportMixin("kameloso.plugins.quotes", "QuotesPlugin"));
+
+
+    version(Posix)
+    {
+        mixin(tryImportMixin("kameloso.plugins.pipeline", "PipelinePlugin"));
+    }
+    else
+    {
+        // We need to do this so as to let `EnabledPosixPlugins` below be able to
+        // resolve `PipelinePlugin`.
+        alias PipelinePlugin = AliasSeq!();
+    }
+
+
+    version(Web)
+    {
+        mixin(tryImportMixin("kameloso.plugins.webtitles", "WebtitlesPlugin"));
+        mixin(tryImportMixin("kameloso.plugins.bashquotes", "BashQuotesPlugin"));
+        mixin(tryImportMixin("kameloso.plugins.reddit", "RedditPlugin"));
+    }
+    else
+    {
+        // Likewise we need to do this so as to let `EnabledWebPlugins` below  be
+        // able to resolve these plugins.
+        alias WebtitlesPlugin = AliasSeq!();
+        alias BashQuotesPlugin = AliasSeq!();
+        alias RedditPlugin = AliasSeq!();
+    }
+
+
+    version(TwitchSupport)
+    {
+        mixin(tryImportMixin("kameloso.plugins.twitch", "TwitchService"));
+    }
+    else
+    {
+        // Likewise we need to do this so as to let `EnabledPlugins` below be able
+        // to resolve `TwitchService`.
+        public alias TwitchService = AliasSeq!();
+    }
 }
 
-version(TwitchSupport)
-{
-    // Import real TwitchService
-    public import kameloso.plugins.twitch;
-}
-else
-{
-    alias TwitchService = AliasSeq!();
-}
 
 /++
  +  List of enabled plugins. Add and remove to enable and disable.
  +
+ +  Due to use of `tryImportMixin` above only files actually present will have
+ +  been imported.
+ +
  +  Note that `dub` will still compile any files in the `plugins` directory!
  +  To completely omit a plugin you will either have to compile the bot
- +  manually, or add an `__EOF__` at the top of the plugin source file.
+ +  manually, delete the source file(s) in question, or add an `__EOF__` at the
+ +  top of them. (Everything a line with that text is skipped.)
  +/
+import std.meta : AliasSeq;
 version(WithPlugins)
 {
     public alias EnabledPlugins = AliasSeq!(
@@ -73,9 +135,21 @@ version(WithPlugins)
         AutomodePlugin,
         QuotesPlugin,
     );
+
+    public alias EnabledWebPlugins = AliasSeq!(
+        WebtitlesPlugin,
+        RedditPlugin,
+        BashQuotesPlugin,
+    );
+
+    public alias EnabledPosixPlugins = AliasSeq!(
+        PipelinePlugin,
+    );
 }
 else
 {
     // Not compiling in any plugins, so don't list any.
     public alias EnabledPlugins = AliasSeq!();
+    public alias EnabledWebPlugins = AliasSeq!();
+    public alias EnabledPosixPlugins = AliasSeq!();
 }
