91865e9fc7737325dad64bc25d30152d34c32533
JR <zorael@gmail.com>
2018-10-26 17:06:18 +0200

    apt-get install, not just apt-get

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -9,7 +9,7 @@ install_deps() {
 
     # E: The method driver /usr/lib/apt/methods/https could not be found.
     # N: Is the package apt-transport-https installed?
-    sudo apt-get -y apt-transport-https
+    sudo apt-get install -y apt-transport-https
 
     # fingerprint 0xEBCF975E5BA24D5E
     sudo apt-get -y --allow-unauthenticated install --reinstall d-apt-keyring
