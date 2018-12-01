6ce9ab225cec8deffdd35561a9e23279ff96f5f6
JR <zorael@gmail.com>
2018-12-01 20:27:07 +0100

    More or less complete Twitch tag coverage
    Unsure of whether it's a smart or a bad idea to replace some of the
    cases with checks of what the strings start with (beginsWith("bad_"),
    beginsWith("usage_")). Do it like this for now, profile and see.

--- source/kameloso/plugins/twitchsupport.d
+++ source/kameloso/plugins/twitchsupport.d
@@ -108,6 +108,7 @@ void parseTwitchTags(TwitchSupportService service, ref IRCEvent event)
             // Msg-id Tags for the NOTICE Commands Capability.
             // https://dev.twitch.tv/docs/irc#msg-id-tags-for-the-notice-commands-capability
             // https://swiftyspiffy.com/TwitchLib/Client/_msg_ids_8cs_source.html
+            // https://dev.twitch.tv/docs/irc/msg-id/
 
             /*
                 sub
@@ -188,13 +189,35 @@ void parseTwitchTags(TwitchSupportService service, ref IRCEvent event)
                 event.type = Type.TWITCH_RAID;
                 break;
 
-            case "ban_success":
-            case "timeout_success":
-            case "unban_success":
-                // Generic Twitch server reply.
-                event.type = Type.TWITCH_REPLY;
-                event.aux = value;
-                break;
+            /*case "bad_ban_admin":
+            case "bad_ban_anon":
+            case "bad_ban_broadcaster":
+            case "bad_ban_global_mod":
+            case "bad_ban_mod":
+            case "bad_ban_self":
+            case "bad_ban_staff":
+            case "bad_commercial_error":
+            case "bad_delete_message_broadcaster":
+            case "bad_delete_message_mod":
+            case "bad_host_error":
+            case "bad_host_hosting":
+            case "bad_host_rate_exceeded":
+            case "bad_host_rejected":
+            case "bad_host_self":
+            case "bad_marker_client":
+            case "bad_mod_banned":
+            case "bad_mod_mod":
+            case "bad_slow_duration":
+            case "bad_timeout_admin":
+            case "bad_timeout_broadcaster":
+            case "bad_timeout_duration":
+            case "bad_timeout_global_mod":
+            case "bad_timeout_mod":
+            case "bad_timeout_self":
+            case "bad_timeout_staff":
+            case "bad_unban_no_ban":
+            case "bad_unhost_error":
+            case "bad_unmod_mod":*/
 
             case "already_banned":
             case "already_emote_only_on":
@@ -203,13 +226,49 @@ void parseTwitchTags(TwitchSupportService service, ref IRCEvent event)
             case "already_r9k_off":
             case "already_subs_on":
             case "already_subs_off":
-            case "bad_host_hosting":
-            case "bad_unban_no_ban":
+            case "host_tagline_length_error":
+            case "invalid_user":
+            case "msg_bad_characters":
+            case "msg_channel_blocked":
+            case "msg_r9k":
+            case "msg_ratelimit":
+            case "msg_rejected_mandatory":
+            case "msg_room_not_found":
+            case "msg_suspended":
+            case "msg_timedout":
+            case "no_help":
+            case "no_mods":
+            case "not_hosting":
+            case "no_permission":
+            case "raid_already_raiding":
+            case "raid_error_forbidden":
+            case "raid_error_self":
+            case "raid_error_too_many_viewers":
+            case "raid_error_unexpected":
+            case "timeout_no_timeout":
+            case "unraid_error_no_active_raid":
+            case "unraid_error_unexpected":
             case "unrecognized_cmd":
             case "unsupported_chatrooms_cmd":
-            case "msg_room_not_found":
-            case "no_permission":
-            case "raid_error_self":
+            case "untimeout_banned":
+            case "whisper_banned":
+            case "whisper_banned_recipient":
+            case "whisper_restricted_recipient":
+            case "whisper_invalid_args":
+            case "whisper_invalid_login":
+            case "whisper_invalid_self":
+            case "whisper_limit_per_min":
+            case "whisper_limit_per_sec":
+            case "whisper_restricted":
+            case "msg_subsonly":
+            case "msg_verified_email":
+            case "msg_slowmode":
+            case "tos_ban":
+            case "msg_channel_suspended":
+            case "msg_banned":
+            case "msg_duplicate":
+            case "msg_facebook":
+            case "turbo_only_color":
                 // Generic Twitch error.
                 event.type = Type.TWITCH_ERROR;
                 event.aux = value;
@@ -225,22 +284,68 @@ void parseTwitchTags(TwitchSupportService service, ref IRCEvent event)
             case "subs_off":
             case "followers_on":
             case "followers_off":
-            case "followers_on_zero":
+            case "followers_onzero":
+            case "host_on":
+            case "host_off":
+            case "host_target_went_offline":
+            case "host_success":
                 // Generic Twitch settings change
                 event.type = Type.TWITCH_SETTING;
                 event.aux = value;
                 break;
 
-            case "host_on":
-            case "host_target_went_offline":
-            case "host_off":
-                // :tmi.twitch.tv NOTICE #chocotaco :Exited host mode."
+            /*case "usage_ban":
+            case "usage_clear":
+            case "usage_color":
+            case "usage_commercial":
+            case "usage_disconnect":
+            case "usage_emote_only_off":
+            case "usage_emote_only_on":
+            case "usage_followers_off":
+            case "usage_followers_on":
+            case "usage_help":
+            case "usage_host":
+            case "usage_marker":
+            case "usage_me":
+            case "usage_mod":
+            case "usage_mods":
+            case "usage_r9k_off":
+            case "usage_r9k_on":
+            case "usage_raid":
+            case "usage_slow_off":
+            case "usage_slow_on":
+            case "usage_subs_off":
+            case "usage_subs_on":
+            case "usage_timeout":
+            case "usage_unban":
+            case "usage_unhost":
+            case "usage_unmod":
+            case "usage_unraid":
+            case "usage_untimeout":*/
+
+            case "host_success_viewers":
             case "hosts_remaining":
-            case "msg_channel_suspended":
-            case "color_changed":
-            case "room_mods":
+            case "mod_success":
+            case "msg_emotesonly":
+            case "msg_followersonly":
+            case "msg_followersonly_followed":
+            case "msg_followersonly_zero":
+            case "msg_rejected":  // "being checked by mods"
             case "raid_notice_mature":
-                // Twitch notices; what type should they be?
+            case "raid_notice_restricted_chat":
+            case "room_mods":
+            case "timeout_success":
+            case "unban_success":
+            case "unmod_success":
+            case "unraid_success":
+            case "untimeout_success":
+            case "cmds_available":
+            case "color_changed":
+            case "commercial_success":
+            case "delete_message_success":
+            case "ban_success":
+                // Generic Twitch server reply.
+                event.type = Type.TWITCH_REPLY;
                 event.aux = value;
                 break;
 
@@ -253,6 +358,21 @@ void parseTwitchTags(TwitchSupportService service, ref IRCEvent event)
                 break;
 
             default:
+                import kameloso.string : beginsWith;
+
+                event.aux = value;
+
+                if (value.beginsWith("bad_"))
+                {
+                    event.type = Type.TWITCH_ERROR;
+                    break;
+                }
+                else if (value.beginsWith("usage_"))
+                {
+                    event.type = Type.TWITCH_REPLY;
+                    break;
+                }
+
                 version(TwitchWarnings)
                 {
                     import kameloso.terminal : TerminalToken;
@@ -262,6 +382,8 @@ void parseTwitchTags(TwitchSupportService service, ref IRCEvent event)
                 break;
             }
             break;
+
+        ////////////////////////////////////////////////////////////////////////
 
          case "display-name":
             // The user’s display name, escaped as described in the IRCv3 spec.
