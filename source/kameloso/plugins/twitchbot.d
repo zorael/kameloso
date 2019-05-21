cca88ae885a463ff924670ccb3f6d31371394b13
JR <zorael@gmail.com>
2019-05-21 12:01:37 +0200

    Add TwitchBotSettings.bannedPhrasesObeyCase
    To be used to toggle case sensitivity of phrase-matching.

--- source/kameloso/plugins/twitchbot.d
+++ source/kameloso/plugins/twitchbot.d
@@ -39,6 +39,9 @@ struct TwitchBotSettings
     /// Whether or not to do reminders at the end of vote durations.
     bool voteReminders = true;
 
+    /// Whether or not to match ban phrases case-sensitively.
+    bool bannedPhrasesObeyCase = true;
+
     /// How long a user should be timed out if they send a banned phrase.
     int bannedPhraseTimeout = 60;
 }
