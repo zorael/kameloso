79ea8df71f0a8a8b16b30fc858c1496f176ec023
JR <zorael@gmail.com>
2019-05-26 23:52:33 +0200

    Warn on BotCommands missing Description

--- source/kameloso/plugins/common.d
+++ source/kameloso/plugins/common.d
@@ -1845,6 +1845,12 @@ mixin template IRCPluginImpl(bool debug_ = false, string module_ = __MODULE__)
                             }
                         }
                     }
+                    else
+                    {
+                        import std.format : format;
+                        pragma(msg, `Warning: %s.%s is missing a Description annotation for command "%s"`
+                            .format(module_, __traits(identifier, fun), commandUDA.string_));
+                    }
                 }
             }
 
