dbb48d8fe763aeed48e69873bb1abcdefef6d992
JR <zorael@gmail.com>
2019-06-12 00:19:02 +0200

    Add some short comments to hello.d

--- source/kameloso/plugins/hello.d
+++ source/kameloso/plugins/hello.d
@@ -6,10 +6,9 @@ import kameloso.plugins.common;
 import kameloso.irc.defs;
 import kameloso.messaging;
 
-@(IRCEvent.Type.CHAN)
-@(PrivilegeLevel.ignore)
-@BotCommand(PrefixPolicy.nickname, "hello")
-@Description("Says hello")
+@(IRCEvent.Type.CHAN)     // This function should trigger on channel messages
+@(PrivilegeLevel.ignore)  // ...sent by anyone, ignoring whether they're whitelisted or not etc
+@BotCommand(PrefixPolicy.nickname, "hello")  // ...on the command "[bot nickname]: hello"
 void onCommandHi(HelloPlugin plugin, const IRCEvent event)
 {
     chan(plugin.state, event.channel, "Hello World!");
