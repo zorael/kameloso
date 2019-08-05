a81ec1415eacc05fdbacaf7a0dc0799edc61c3e0
JR <zorael@gmail.com>
2019-08-05 19:07:53 +0200

    Add IRCEvent.Type.TWITCH_BULKGIFT
    TWITCH_SUBMYSTERYGIFT reimagined, now that we know what it is. (One-time
    multiple gifts to more than one participant in one go.)

--- source/kameloso/irc/defs.d
+++ source/kameloso/irc/defs.d
@@ -128,6 +128,7 @@ struct IRCEvent
         TWITCH_GIFTUPGRADE,/// Twitch "continuing the gift sub"
         TWITCH_SUBUPGRADE,/// Twitch, someone paying to upgrade their subscription.
         TWITCH_CHARITY,   /// Twitch charity donation.
+        TWITCH_BULKGIFT,  /// Twitch no-target multiple gift event.
 
         RPL_WELCOME, // = 001,          // ":Welcome to <server name> <user>"
         RPL_YOURHOST, // = 002,         // ":Your host is <servername>, running version <version>"
