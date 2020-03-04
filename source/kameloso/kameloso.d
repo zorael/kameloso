99b174c60ee59034004d7b1a081d66877265e874
JR <zorael@gmail.com>
2020-03-04 13:49:06 +0100

    Break out connection summary-printing into own function

--- source/kameloso/kameloso.d
+++ source/kameloso/kameloso.d
@@ -2324,30 +2324,7 @@ int initBot(string[] args)
     // The connection history may be empty if exitSummary was set mid-execution.
     if (settings.exitSummary && instance.connectionHistory.length)
     {
-        import std.stdio : writefln;
-        import core.time : Duration;
-
-        Duration totalTime;
-
-        logger.info("Connection summary");
-
-        foreach (immutable i, const entry; instance.connectionHistory)
-        {
-            import std.datetime.systime : SysTime;
-            import core.time : msecs;
-
-            auto start = SysTime.fromUnixTime(entry.startTime);
-            start.fracSecs = 0.msecs;
-            auto stop = SysTime.fromUnixTime(entry.stopTime);
-            stop.fracSecs = 0.msecs;
-            immutable duration = (stop - start);
-            totalTime += duration;
-
-            writefln("%2d: %s, %d events parsed (%s to %s)",
-                i+1, duration, entry.numEvents, start, stop);
-        }
-
-        logger.info("Total time connected: ", logtint, totalTime);
+        instance.printSummary();
     }
 
     if (*instance.abort)
@@ -2362,4 +2339,55 @@ int initBot(string[] args)
     }
 
     return attempt.retval;
+}
+
+
+// printSummary
+/++
+ +  Prints a summary of the connection(s) made and events parsed this execution.
+ +
+ +  Params:
+ +      instance = Reference to the current `kameloso.common.Kameloso`.
+ +/
+void printSummary(const ref Kameloso instance)
+{
+    import std.stdio : writefln;
+    import core.time : Duration;
+
+    Duration totalTime;
+
+    logger.trace("---");
+    logger.info("Connection summary:");
+
+    string logtint;
+
+    version(Colours)
+    {
+        if (!settings.monochrome)
+        {
+            import kameloso.logger : KamelosoLogger;
+            import kameloso.terminal : TerminalForeground, colour;
+
+            logtint = (cast(KamelosoLogger)logger).logtint;
+        }
+    }
+
+    foreach (immutable i, const entry; instance.connectionHistory)
+    {
+        import std.datetime.systime : SysTime;
+        import core.time : msecs;
+
+        auto start = SysTime.fromUnixTime(entry.startTime);
+        start.fracSecs = 0.msecs;
+        auto stop = SysTime.fromUnixTime(entry.stopTime);
+        stop.fracSecs = 0.msecs;
+        immutable duration = (stop - start);
+        totalTime += duration;
+
+        writefln("%2d: %s, %d events parsed (%s to %s)",
+            i+1, duration, entry.numEvents, start, stop);
+    }
+
+    logger.info("Total time connected: ", logtint, totalTime);
+    logger.trace("---");
 }
