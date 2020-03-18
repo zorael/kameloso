e00ab44cacaee9fe48777af0087651b5a51c0307
JR <zorael@gmail.com>
2020-03-18 17:51:08 +0100

    More legible comment spacing in hello.d

--- source/kameloso/plugins/hello.d
+++ source/kameloso/plugins/hello.d
@@ -6,9 +6,9 @@ import kameloso.plugins.common;
 import kameloso.messaging;
 import dialect.defs;
 
-@(IRCEvent.Type.CHAN)     // This function should automatically trigger on channel messages
-@(PrivilegeLevel.ignore)  // ...sent by anyone...
-@BotCommand(PrefixPolicy.prefixed, "hello")  // ...on "!hello"
+@(IRCEvent.Type.CHAN)                        // This function should automatically trigger on channel messages
+@(PrivilegeLevel.ignore)                     // ...sent by anyone...
+@BotCommand(PrefixPolicy.prefixed, "hello")  // ...saying "!hello"
 void onCommandHello(HelloPlugin plugin, const IRCEvent event)
 {
     chan(plugin.state, event.channel, "Hello World!");
