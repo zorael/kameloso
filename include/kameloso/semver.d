098a718e7169a6d314621d656262a06d237020bd
JR <zorael@gmail.com>
2020-03-31 22:50:24 +0200

    Add kameloso.semver in include/kameloso/semver.d

---
+++ include/kameloso/semver.d
@@ -0,0 +1,19 @@
+/++
+ +  SemVer information about the current release.
+ +
+ +  Contains only definitions, no code. Helps importing projects tell what
+ +  features are available.
+ +/
+module kameloso.semver;
+
+
+/// SemVer versioning of this build.
+enum KamelosoSemVer
+{
+    majorVersion = 1,  /// SemVer major version of the program.
+    minorVersion = 7,  /// SemVer minor version of the program.
+    patchVersion = 0,  /// SemVer patch version of the program.
+}
+
+/// Pre-release SemVer subversion of this build.
+enum KamelosoSemVerPrerelease = string.init;
