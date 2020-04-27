271a75e74ad178a6e65f52439a63dfd0797d0738
JR <zorael@gmail.com>
2020-04-27 11:50:36 +0200

    Move getSymbolsByUDA warning to core.d
    It's where getSymbolsByUDA is being used.

--- source/kameloso/plugins/common.d
+++ source/kameloso/plugins/common.d
@@ -20,40 +20,8 @@ import std.typecons : Flag, No, Yes;
  +/
 public import kameloso.plugins.core : IRCPluginState;
 
-
 //version = TwitchWarnings;
 //version = ExplainRepeat;
-
-
-/++
- +  2.079.0 has a bug that breaks plugin processing completely. It's fixed in
- +  patch .1 (2.079.1), but there's no API for knowing the patch number.
- +
- +  Infer it by testing for the broken behaviour and warn (during compilation).
- +/
-static if (__VERSION__ == 2079L)
-{
-    import lu.traits : getSymbolsByUDA;
-
-    struct UDA_2079 {}
-    struct Foo_2079
-    {
-        @UDA_2079
-        {
-            int i;
-            void fun() {}
-            int n;
-        }
-    }
-
-    static if (getSymbolsByUDA!(Foo_2079, UDA_2079).length != 3)
-    {
-        pragma(msg, "WARNING: You are using a `2.079.0` compiler with a broken " ~
-            "crucial trait in its standard library. The program will not " ~
-            "function normally. Please upgrade to `2.079.1` or later.");
-    }
-}
-
 
 public:
 
