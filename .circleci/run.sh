c91ef0cf68997a60f9791918c974c5541f014e39
JR <zorael@gmail.com>
2020-05-27 13:24:58 +0200

    CIs: no longer clone lu manually

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -15,10 +15,10 @@ install_deps() {
     sudo apt update
     sudo apt install -y --allow-unauthenticated dmd-compiler dub libcurl4-openssl-dev
 
-    git clone https://github.com/zorael/lu.git
+    #git clone https://github.com/zorael/lu.git
     #git clone https://github.com/zorael/dialect.git
 
-    dub add-local lu
+    #dub add-local lu
     #dub add-local dialect
 }
 
