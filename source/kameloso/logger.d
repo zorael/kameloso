7b87e88ffac123bb5c67e25f3243a1778688d7db
JR <zorael@gmail.com>
2020-05-29 21:12:51 +0200

    KamelosoLogger: don't inherit from Logger

--- source/kameloso/logger.d
+++ source/kameloso/logger.d
@@ -2,12 +2,9 @@
  +  Contains the custom `KamelosoLogger` class, used to print timestamped and
  +  (optionally) coloured logging messages.
  +
- +  This is merely a subclass of `std.experimental.logger.Logger` that formats
- +  its arguments differently, implying the log level by way of colours.
- +
  +  Example:
  +  ---
- +  Logger logger = new KamelosoLogger;
+ +  auto logger = new KamelosoLogger(No.monochrome, No.brigtTerminal, Yes.flush);
  +
  +  logger.log("This is LogLevel.log");
  +  logger.info("LogLevel.info");
@@ -21,12 +18,9 @@ module kameloso.logger;
 
 private:
 
-import std.experimental.logger.core : Logger;
 import std.range.primitives : isOutputRange;
 
 public:
-
-@safe:
 
 /+
     Build tint colours at compile time, saving the need to compute them during
@@ -37,23 +31,17 @@ version = CtTints;
 
 // KamelosoLogger
 /++
- +  Modified `std.experimental.logger.Logger` to print timestamped and coloured logging messages.
+ +  Logger class, used to print timestamped and coloured logging messages.
  +
- +  It is thread-local so instantiate more if you're threading. Even so there
- +  may be race conditions.
- +
- +  See_Also:
- +      std.experimental.logger.Logger.
+ +  It is thread-local so instantiate more if you're threading.
  +/
-final class KamelosoLogger : Logger
+final class KamelosoLogger
 {
-@safe:
-
 private:
     import std.array : Appender;
-    import std.concurrency : Tid;
-    import std.datetime.systime : SysTime;
     import std.experimental.logger : LogLevel;
+    import std.format : format;
+    import std.traits : EnumMembers;
     import std.typecons : Flag, No, Yes;
 
     /// Buffer to compose a line in before printing it to screen.
@@ -61,6 +49,10 @@ private:
 
     /// The initial size to allocate for `linebuffer`. It will grow if needed.
     enum linebufferInitialSize = 4096;
+
+    bool monochrome;  /// Whether to use colours or not in logger output.
+    bool brightTerminal;   /// Whether or not to use colours for a bright background.
+    bool flush;  /// Whether or not we should flush stdout after finishing writing to it.
 
 public:
     version(Colours)
@@ -72,22 +64,17 @@ public:
         alias logcoloursDark = DefaultColours.logcoloursDark;
     }
 
-    bool monochrome;  /// Whether to use colours or not in logger output.
-    bool brightTerminal;   /// Whether or not to use colours for a bright background.
-    bool flush;  /// Whether or not we should flush stdout after finishing writing to it.
-
     /// Create a new `KamelosoLogger` with the passed settings.
-    this(LogLevel lv,
-        const Flag!"monochrome" monochrome,
+    this(const Flag!"monochrome" monochrome,
         const Flag!"brightTerminal" brightTerminal,
-        const Flag!"flush" flush)
+        const Flag!"flush" flush) @safe
     {
         linebuffer.reserve(linebufferInitialSize);
         this.monochrome = monochrome;
         this.brightTerminal = brightTerminal;
         this.flush = flush;
-        super(lv);
     }
+
 
     // tint
     /++
@@ -142,6 +129,7 @@ public:
                 .format(tintDark, tintDarkTable));
         }
     }
+
 
     // tintImpl
     /++
@@ -205,117 +193,60 @@ public:
         auto fataltint() const @property @nogc { return tintImpl!(LogLevel.fatal); }
     }
 
-    /++
-     +  This override is needed or it won't compile.
-     +
-     +  Params:
-     +      payload = Message payload to write.
-     +/
-    override void writeLogMsg(ref LogEntry payload) pure nothrow const @nogc {}
 
     /++
      +  Outputs the header of a logger message.
      +
-     +  Overload that takes an output range sink.
+     +  Params:
+     +      logLevel = The `std.experimental.logger.LogLevel` to treat this
+     +          message as being of.
      +/
-    pragma(inline)
-    protected void beginLogMsg(Sink)(auto ref Sink sink, string, int, string,
-        string, string, LogLevel thisLogLevel, Tid, SysTime timestamp, Logger) const
-    if (isOutputRange!(Sink, char[]))
+    protected void beginLogMsg(const LogLevel logLevel) @safe
     {
         import std.datetime : DateTime;
+        import std.datetime.systime : Clock;
 
         version(Colours)
         {
             if (!monochrome)
             {
                 alias Timestamp = DefaultColours.TimestampColour;
-                sink.colourWith(brightTerminal ? Timestamp.bright : Timestamp.dark);
+                linebuffer.colourWith(brightTerminal ? Timestamp.bright : Timestamp.dark);
             }
         }
 
-        sink.put('[');
-        (cast(DateTime)timestamp).timeOfDay.toString(sink);
-        sink.put("] ");
+        linebuffer.put('[');
+        (cast(DateTime)Clock.currTime).timeOfDay.toString(linebuffer);
+        linebuffer.put("] ");
 
         if (monochrome) return;
 
         version(Colours)
         {
-            sink.colourWith(brightTerminal ?
-                logcoloursBright[thisLogLevel] :
-                logcoloursDark[thisLogLevel]);
+            linebuffer.colourWith(brightTerminal ?
+                logcoloursBright[logLevel] :
+                logcoloursDark[logLevel]);
         }
     }
 
-    /++
-     +  Outputs the header of a logger message.
-     +
-     +  Overload that passes `linebuffer` to the other `beginLogMsg`.
-     +/
-    override protected void beginLogMsg(string file, int line, string funcName,
-        string prettyFuncName, string moduleName, LogLevel logLevel,
-        Tid threadId, SysTime timestamp, Logger logger)
-    {
-        return beginLogMsg(linebuffer, file, line, funcName,
-            prettyFuncName, moduleName, logLevel, threadId, timestamp, logger);
-    }
-
-    /++
-     +  Outputs the message part of a logger message; the content.
-     +
-     +  Overload that takes an output range sink.
-     +/
-    pragma(inline)
-    protected void logMsgPart(Sink)(auto ref Sink sink, const(char)[] msg)
-    if (isOutputRange!(Sink, char[]))
-    {
-        sink.put(msg);
-    }
-
-    /++
-     +  Outputs the message part of a logger message; the content.
-     +
-     +  Overload that passes `linebuffer` to the other `logMsgPart`.
-     +/
-    override protected void logMsgPart(scope const(char)[] msg)
-    {
-        if (!msg.length) return;
-
-        return logMsgPart(linebuffer, msg);
-    }
 
     /++
      +  Outputs the tail of a logger message.
-     +
-     +  Overload that takes an output range sink.
-     +/
-    pragma(inline)
-    version(Colours)
-    protected void finishLogMsg(Sink)(auto ref Sink sink) const
-    if (isOutputRange!(Sink, char[]))
-    {
-        if (!monochrome)
-        {
-            // Reset.blink in case a fatal message was thrown
-            sink.colourWith(TerminalForeground.default_, TerminalReset.blink);
-        }
-    }
-
-    /++
-     +  Outputs the tail of a logger message.
-     +  Overload that passes `linebuffer` to the other `finishLogMsg`.
      +
      +  @trusted to allow us to flush `stdout`. `std.stdio.writeln` seems to
      +  do this and it's annotated @trusted, so just mimic that.
      +/
-    override protected void finishLogMsg() @trusted
+    private void finishLogMsg() @trusted
     {
         import std.stdio : stdout, writeln;
 
         version(Colours)
         {
-            finishLogMsg(linebuffer);
+            if (!monochrome)
+            {
+                // Reset.blink in case a fatal message was thrown
+                linebuffer.colourWith(TerminalForeground.default_, TerminalReset.blink);
+            }
         }
 
         writeln(linebuffer.data);
@@ -323,6 +254,185 @@ public:
 
         if (flush) stdout.flush();
     }
+
+
+    // printImpl
+    /++
+     +  Prints a timestamped log message to screen. Implementation function.
+     +
+     +  Prints the arguments as they are if possible (if they are some variant of
+     +  `char` or `char[]`), and otherwise tries to coerce them by using
+     +  `std.conv.to`.
+     +
+     +  Params:
+     +      logLevel = The `std.experimental.logger.LogLevel` to treat this
+     +          message as being of.
+     +      args = Variadic arguments to compose the output message with.
+     +/
+    private void printImpl(Args...)(const LogLevel logLevel, Args args)
+    {
+        beginLogMsg(logLevel);
+
+        foreach (arg; args)
+        {
+            alias T = typeof(arg);
+
+            static if (is(T : string) || is(T : char[]) || is(T : char))
+            {
+                linebuffer.put(arg);
+            }
+            else static if (is(T == enum))
+            {
+                import lu.conv : Enum;
+                linebuffer.put(Enum!T.toString(arg));
+            }
+            else static if (__traits(compiles, arg.toString(linebuffer)))
+            {
+                arg.toString(linebuffer);
+            }
+            /*else static if (isSomeFunction!(arg.toString) && is(ReturnType!(arg.toString) : string))
+            {
+                linebuffer.put(arg.toString);
+            }*/
+            else static if ((is(T == struct) || is(T == class) || is(T == interface)) &&
+                is(typeof(T.toString)))
+            {
+                import std.traits : isSomeFunction;
+
+                static if (isSomeFunction!(T.toString))
+                {
+                    import std.traits : ReturnType;
+
+                    static if (__traits(compiles, arg.toString(linebuffer)))
+                    {
+                        arg.toString(linebuffer);
+                    }
+                    else static if (is(ReturnType!(T.toString) : string))
+                    {
+                        linebuffer.put(arg.toString);
+                    }
+                    else
+                    {
+                        import std.conv : to;
+
+                        // std.conv.to fallback
+                        linebuffer.put(arg.to!string);
+                    }
+                }
+                else static if (__traits(isTemplate, T.toString) &&
+                    __traits(compiles, arg.toString(linebuffer)))
+                {
+                    arg.toString(linebuffer);
+                }
+                else
+                {
+                    import std.conv : to;
+
+                    // std.conv.to fallback
+                    linebuffer.put(arg.to!string);
+                }
+            }
+            else
+            {
+                pragma(msg, T.stringof);
+                pragma(msg, is(T == struct));
+                pragma(msg, is(T == class));
+                pragma(msg, __traits(isTemplate, T));
+                import std.conv : to;
+                linebuffer.put(arg.to!string);
+            }
+        }
+
+        finishLogMsg();
+    }
+
+
+    // printfImpl
+    /++
+     +  Prints a timestamped log message to screen as per the passed runtime pattern,
+     +  in `printf` style. Implementation function.
+     +
+     +  Uses `std.format.formattedWrite` to coerce the passed arguments as
+     +  the format pattern dictates.
+     +
+     +  Params:
+     +      logLevel = The `std.experimental.logger.LogLevel` to treat this
+     +          message as being of.
+     +      pattern = Runtime pattern to format the output with.
+     +      args = Variadic arguments to compose the output message with.
+     +/
+    private void printfImpl(Args...)(const LogLevel logLevel, const string pattern, Args args)
+    {
+        import std.format : formattedWrite;
+
+        beginLogMsg(logLevel);
+        linebuffer.formattedWrite(pattern, args);
+        finishLogMsg();
+    }
+
+
+    // printfImpl
+    /++
+     +  Prints a timestamped log message to screen as per the passed compile-time pattern,
+     +  in `printf` style. Implementation function.
+     +
+     +  Uses `std.format.formattedWrite` to coerce the passed arguments as
+     +  the format pattern dictates.
+     +
+     +  If on D version 2.074 or later, passes the pattern as a compile-time
+     +  parameter to it, to validate that the pattern matches the arguments.
+     +  If earlier it passes execution to the other, runtime-pattern `printfImpl` overload.
+     +
+     +  Params:
+     +      pattern = Compile-time pattern to validate the arguments and format the output with.
+     +      logLevel = The `std.experimental.logger.LogLevel` to treat this
+     +          message as being of.
+     +      args = Variadic arguments to compose the output message with.
+     +/
+    private void printfImpl(string pattern, Args...)(const LogLevel logLevel, Args args)
+    {
+        static if (__VERSION__ >= 2074)
+        {
+            import std.format : formattedWrite;
+
+            beginLogMsg(logLevel);
+            linebuffer.formattedWrite!pattern(args);
+            finishLogMsg();
+        }
+        else
+        {
+            printfImpl(logLevel, pattern, args);
+        }
+    }
+
+
+    /+
+     +  Generate `trace`, `tracef`, `log`, `logf` and similar Logger-esque functions.
+     +/
+    static foreach (const lv; [ EnumMembers!LogLevel ])
+    {
+        mixin(
+q{void %1$s(Args...)(Args args)
+{
+    printImpl(LogLevel.%1$s, args);
+}
+
+void %1$sf(Args...)(const string pattern, Args args)
+{
+    printfImpl(LogLevel.%1$s, pattern, args);
+}
+
+void %sf(string pattern, Args...)(Args args)
+{
+    printfImpl!pattern(LogLevel.%1$s, args);
+}}.format(lv));
+    }
+
+    /// Alias to `KamelosoLogger.all`.
+    alias log = all;
+
+    /// Alias to `KamelosoLogger.allf`.
+    alias logf = allf;
 }
 
 ///
@@ -331,7 +441,7 @@ unittest
     import std.experimental.logger : LogLevel;
     import std.typecons : Flag, No, Yes;
 
-    Logger log_ = new KamelosoLogger(LogLevel.all, Yes.monochrome, No.brightTerminal, Yes.flush);
+    auto log_ = new KamelosoLogger(Yes.monochrome, No.brightTerminal, Yes.flush);
 
     log_.log("log: log");
     log_.info("log: info");
@@ -342,7 +452,7 @@ unittest
 
     version(Colours)
     {
-        log_ = new KamelosoLogger(LogLevel.all, No.monochrome, Yes.brightTerminal, Yes.flush);
+        log_ = new KamelosoLogger(No.monochrome, Yes.brightTerminal, Yes.flush);
 
         log_.log("log: log");
         log_.info("log: info");
@@ -351,7 +461,7 @@ unittest
         // log_.fatal("log: FATAL");
         log_.trace("log: trace");
 
-        log_ = new KamelosoLogger(LogLevel.all, No.monochrome, No.brightTerminal, Yes.flush);
+        log_ = new KamelosoLogger(No.monochrome, No.brightTerminal, Yes.flush);
 
         log_.log("log: log");
         log_.info("log: info");
