dd6c6feaa5745c3fa2eb0874c3393a2c049ee362
JR <zorael@gmail.com>
2020-09-07 00:08:32 +0200

    Twitch: version(TwitchAPIFeatures) header in api.d

--- source/kameloso/plugins/twitchbot/api.d
+++ source/kameloso/plugins/twitchbot/api.d
@@ -6,7 +6,10 @@ module kameloso.plugins.twitchbot.api;
 version(WithPlugins):
 version(TwitchSupport):
 version(WithTwitchBotPlugin):
-version(Web):
+
+version(Web) version = TwitchAPIFeatures;
+
+version(TwitchAPIFeatures):
 
 private:
 
