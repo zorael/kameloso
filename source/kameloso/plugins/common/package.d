ea7b982d43c743d1e4016e78a8b34636792781ed
JR <zorael@gmail.com>
2020-09-17 10:22:06 +0200

    Include plugins.common.core in plugins.common.package

--- source/kameloso/plugins/common/package.d
+++ source/kameloso/plugins/common/package.d
@@ -5,6 +5,7 @@ module kameloso.plugins.common;
 
 version(WithPlugins):
 
+public import kameloso.plugins.common.core;
 public import kameloso.plugins.common.base;
 public import kameloso.plugins.common.delayawait;
 public import kameloso.plugins.common.mixins;
