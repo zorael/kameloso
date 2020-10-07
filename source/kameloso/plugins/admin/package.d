b1ffe010d0da500b833cc8c9fc5806fb078e8eb7
JR <zorael@gmail.com>
2020-10-07 17:28:12 +0200

    Better package.d files

--- source/kameloso/plugins/admin/package.d
+++ source/kameloso/plugins/admin/package.d
@@ -1,9 +1,18 @@
 /++
     Package for the Admin plugin modules.
+
+    See_Also:
+        kameloso.plugins.admin.base,
+        kameloso.plugins.admin.classifiers,
+        kameloso.plugins.admin.debugging
  +/
 module kameloso.plugins.admin;
 
 version(WithPlugins):
 version(WithAdminPlugin):
 
-public import kameloso.plugins.admin.base;
+public:
+
+import kameloso.plugins.admin.base;
+import kameloso.plugins.admin.classifiers;
+import kameloso.plugins.admin.debugging;
