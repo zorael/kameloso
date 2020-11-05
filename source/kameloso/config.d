08a752606f5d560160147bb6244c3e8044e80a59
JR <zorael@gmail.com>
2020-11-05 15:09:13 +0100

    Tweak "settings will be lost" message

--- source/kameloso/config.d
+++ source/kameloso/config.d
@@ -815,7 +815,7 @@ void notifyAboutMissingSettings(const string[][string] missingEntries,
     logger.logf("Use %s%s --save%s to regenerate the file, " ~
         "updating it with all available configuration. [%1$s%4$s%3$s]",
         Tint.info, binaryPath.baseName, Tint.log, configFile);
-    logger.warning("Mind that any settings belonging to unbuilt plugins will be LOST.");
+    logger.warning("Mind that any comments and/or sections belonging to unbuilt plugins will be removed.");
     logger.trace();
 }
 
