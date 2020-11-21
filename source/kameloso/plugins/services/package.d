2844bf0ba7f302036b2082298aa6ca6a89bae1e3
JR <zorael@gmail.com>
2020-11-21 03:13:58 +0100

    Add a main package file and one for plugins.services

---
+++ source/kameloso/plugins/services/package.d
@@ -0,0 +1,8 @@
+/++
+    "Service"-level plugins, used to carry out tasks behind the scenes.
+
+    Removing or disabling any of these may yield adverse results.
+ +/
+module kameloso.plugins.services;
+
+// Don't offer anything as public imports, just serve as the frontpage for adrdox.
