15e0a0bf29dacdcdd9991ba56761ff285e37abc2
JR <zorael@gmail.com>
2020-12-05 15:46:02 +0100

    Annotate ConnectionSettings.receiveTimeout `@Hidden`

--- source/kameloso/kameloso.d
+++ source/kameloso/kameloso.d
@@ -707,7 +707,7 @@ public:
     bool ssl = false;
 
     /// Socket receive timeout in milliseconds (how often to check for concurrency messages).
-    uint receiveTimeout = Timeout.receiveMsecs;
+    @Hidden uint receiveTimeout = Timeout.receiveMsecs;
 }
 
 
