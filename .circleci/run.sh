1b33f11fc9bf61fef37f36fca2461df12cc93173
JR <zorael@gmail.com>
2021-11-23 14:17:44 +0100

    Revert "CircleCI: Attempt 32-bit builds with dmd again"
    This reverts commit ede274cc7d319246a4aa8e48176c69b3c9937b47.

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -117,7 +117,7 @@ case $1 in
         #use_lu_master
         #use_dialect_master
 
-        time build dmd x86 "" --build-mode=singleFile
+        #time build dmd x86 "" --build-mode=singleFile  # no 32-bit libs?
         time build dmd x86_64 "" --build-mode=singleFile
         ;;
 
