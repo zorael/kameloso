f98894457e92216f7b8511ec5bd1aa06cc4cd680
JR <zorael@gmail.com>
2021-11-23 17:18:31 +0100

    CircleCI: Appease codefactor.io linting of run.sh

--- .circleci/run.sh
+++ .circleci/run.sh
@@ -52,7 +52,7 @@ use_dialect_master() {
 }
 
 build() {
-    local DC compiler_switch arch_switch build_ext arch_ext options
+    local DC compiler_switch arch_switch build_ext arch_ext
 
     DC="$1"
     [[ "$2" == "x86_64" ]] && arch_ext="" || arch_ext="-$2"
@@ -60,42 +60,41 @@ build() {
     compiler_switch="--compiler=$DC"
     arch_switch="--arch=$2"
 
-    shift 2
-    shift 1 || true
-    options="$@"
+    shift 2  # shift away compiler and arch
+    shift 1 || true  # shift away build extension iff supplied
+    # "$@" is now any extra parameters passed to build
 
     mkdir -p artifacts
     dub clean
 
     ## test
-    time dub test $compiler_switch $arch_switch $options -c unittest${build_ext}
-    options="$options --nodeps"
+    time dub test $compiler_switch $arch_switch "$@" -c unittest${build_ext}
 
 
     ## debug
-    time dub build $compiler_switch $arch_switch $options -b debug -c twitch${build_ext}
+    time dub build $compiler_switch $arch_switch "$@" --nodeps -b debug -c twitch${build_ext}
     mv kameloso "artifacts/kameloso-${DC}${arch_ext}"
 
-    time dub build $compiler_switch $arch_switch $options -b debug -c dev${build_ext}
+    time dub build $compiler_switch $arch_switch "$@" --nodeps -b debug -c dev${build_ext}
     mv kameloso "artifacts/kameloso-$DC-dev${arch_ext}"
 
 
     ## plain
-    #time dub build $compiler_switch $arch_switch $options -b plain -c twitch${build_ext} || true
+    #time dub build $compiler_switch $arch_switch "$@" -b plain -c twitch${build_ext} || true
     #mv kameloso "artifacts/kameloso-$DC-plain${arch_ext}" || \
     #    touch "artifacts/kameloso-$DC-plain${arch_ext}.failed"
 
-    time dub build $compiler_switch $arch_switch $options -b plain -c dev${build_ext} || true
+    time dub build $compiler_switch $arch_switch "$@" -b plain -c dev${build_ext} || true
     #mv kameloso "artifacts/kameloso-$DC-plain-dev${build_ext}${arch_ext}" || \
     #    touch "artifacts/kameloso-$DC-plain-dev${build_ext}${arch_ext}.failed"
 
 
     ## release
-    #time dub build $compiler_switch $arch_switch $options -b release -c twitch${build_ext} || true
+    #time dub build $compiler_switch $arch_switch "$@" -b release -c twitch${build_ext} || true
     #mv kameloso "artifacts/kameloso-$DC-release${arch_ext}" || \
     #    touch "artifacts/kameloso-$DC-release${arch_ext}.failed"
 
-    time dub build $compiler_switch $arch_switch $options -b release -c dev${build_ext} || true
+    time dub build $compiler_switch $arch_switch "$@" -b release -c dev${build_ext} || true
     #mv kameloso "artifacts/kameloso-$DC-release-dev${build_ext}${arch_ext}" || \
     #    touch "artifacts/kameloso-$DC-release-dev${build_ext}${arch_ext}.failed"
 }
