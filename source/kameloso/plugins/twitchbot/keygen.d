863ec50c0a22b10bacbf79783870a245c3c94635
JR <zorael@gmail.com>
2022-02-27 18:13:37 +0100

    Twitch: don't request channel:read:subscriptions scope
    We're not using it (as of yet).

--- source/kameloso/plugins/twitchbot/keygen.d
+++ source/kameloso/plugins/twitchbot/keygen.d
@@ -86,7 +86,7 @@ instructions and log in to authorise the use of this program with your account.
         //"channel:read:predictions",
         //"channel:read:redemptions",
         //"channel:read:stream_key",
-        "channel:read:subscriptions",
+        //"channel:read:subscriptions",
         //"clips:edit",
         //"moderation:read",
         //"moderator:manage:banned_users",
