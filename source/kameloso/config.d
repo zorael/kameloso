d3f067befb9a332f05fc884671c2e78905e62c0c
JR <zorael@gmail.com>
2022-03-31 15:03:22 +0200

    Initial attempt at SSL .url links

--- source/kameloso/config.d
+++ source/kameloso/config.d
@@ -459,6 +459,10 @@ Next handleGetopt(ref Kameloso instance,
         bool shouldShowSettings;
         bool shouldAppendToArrays;
 
+        // Windows-only but must be declared regardless of platform
+        bool shouldDownloadOpenSSL;
+        bool shouldDownloadCacert;
+
         string[] inputGuestChannels;
         string[] inputHomeChannels;
         string[] inputAdmins;
@@ -842,6 +846,72 @@ Next handleGetopt(ref Kameloso instance,
             // --settings was passed, show all options and quit
             if (!settings.headless) printSettings(instance, customSettings);
             return Next.returnSuccess;
+        }
+
+        version(Windows)
+        {
+            if (shouldDownloadCacert || shouldDownloadOpenSSL)
+            {
+                import kameloso.common : logger;
+                import kameloso.platform : cbd = configurationBaseDirectory;
+                import std.process : Pid, wait;
+
+                Pid cacertBrowser;
+                Pid openSSLBrowser;
+
+                scope(exit)
+                {
+                    if (cacertBrowser) wait(cacertBrowser);
+                    if (openSSLBrowser) wait(openSSLBrowser);
+                }
+
+                Pid openURL(const string url)
+                {
+                    import std.file : tempDir;
+                    import std.path : buildNormalizedPath;
+                    import std.process : spawnProcess;
+                    import std.stdio : File;
+
+                    immutable urlFileName = buildNormalizedPath(tempDir, "kameloso", "link.url");
+
+                    {
+                        auto urlFile = File(urlFileName, "w");
+                        urlFile.writeln("[InternetShortcut]\nURL=", url);
+                    }
+
+                    immutable string[2] browserCommand = [ "explorer", urlFileName ];
+                    auto nulFile = File("NUL", "r+");
+                    return spawnProcess(browserCommand[], nulFile, nulFile, nulFile);
+                }
+
+                if (shouldDownloadCacert)
+                {
+                    enum url = "http://curl.se/ca/cacert.pem";
+                    enum pattern = "<l>cacert.pem</>: Save it anywhere, preferably in <l>%s/</>.";
+                    enum pathPattern = "That way you don't have to enter its full path " ~
+                        "in the configuration file (<l>cacert.pem</> will be enough).";
+                    enum configPattern = "(Open the configuration file by passing <l>--gedit</)";
+
+                    logger.infof(pattern.expandTags, cbd);
+                    logger.info(pathPattern.expandTags);
+                    logger.info(configPattern.expandTags);
+
+                    cacertBrowser = openURL(url);
+                }
+
+                if (shouldDownloadOpenSSL)
+                {
+                    enum url = "https://slproweb.com/products/Win32OpenSSL.html";
+                    enum versionPattern = "<l>OpenSSL>/>: You want <l>v1.1.1n Light</> (or later), not <l>v3.0.x</>.";
+                    enum installPattern = "Remember to install to <l>Windows system directories</> when asked.";
+
+                    logger.info(versionPattern.expandTags);
+                    logger.info(installPattern.expandTags);
+                    openSSLBrowser = openURL(url);
+                }
+
+                return Next.returnSuccess;
+            }
         }
 
         return Next.continue_;
