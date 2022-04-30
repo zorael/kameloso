553e124e68e38c069d061186d17a1edb0c641f5d
JR <zorael@gmail.com>
2022-04-30 11:42:44 +0200

    Rip out Twitch timers

--- source/kameloso/plugins/twitchbot/package.d
+++ source/kameloso/plugins/twitchbot/package.d
@@ -4,7 +4,6 @@
     See_Also:
         [kameloso.plugins.twitchbot.base|twitchbot.base]
         [kameloso.plugins.twitchbot.api|twitchbot.api]
-        [kameloso.plugins.twitchbot.timers|twitchbot.timers]
         [kameloso.plugins.twitchbot.keygen|twitchbot.keygen]
  +/
 module kameloso.plugins.twitchbot;
@@ -16,5 +15,4 @@ public:
 
 import kameloso.plugins.twitchbot.base;
 import kameloso.plugins.twitchbot.api;
-import kameloso.plugins.twitchbot.timers;
 //import kameloso.plugins.twitchbot.keygen;  // Only necessary from within onCAP
