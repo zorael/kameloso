72b58cb2a05fae4fa5528e5da9a09b3e8d1ef3c1
JR <zorael@gmail.com>
2023-01-24 21:34:27 +0100

    Add `kameloso.messaging.reply`
    Replies to a channel message, on Twitch.

--- source/kameloso/messaging.d
+++ source/kameloso/messaging.d
@@ -184,6 +184,69 @@ unittest
 }
 
 
+// reply
+/++
+    Replies to a message in a Twitch channel. Requires version `TwitchSupport`.
+
+    Params:
+        priority = Whether or not to send the message as a priority message,
+            received before other messages are, if there are several.
+        state = The current plugin's [kameloso.plugins.common.core.IRCPluginState|IRCPluginState],
+            via which to send messages to the server.
+        channelName = Channel in which to send the message.
+        id = Twitch message ID of original message.
+        content = Message body content to send.
+        quiet = Whether or not to echo what was sent to the local terminal.
+        background = Whether or not to send it as a low-priority background message.
+        caller = String name of the calling function, or something else that gives context.
+ +/
+version(TwitchSupport)
+void reply(Flag!"priority" priority = No.priority)
+    (IRCPluginState state,
+    const string channelName,
+    const string id,
+    const string content,
+    const Flag!"quiet" quiet = No.quiet,
+    const Flag!"background" background = No.background,
+    const string caller = __FUNCTION__)
+in (channelName.length, "Tried to reply to a channel message but no channel was given")
+in (id.length, "Tried to reply to a channel message but no message ID was given")
+{
+    static if (priority) import std.concurrency : send = prioritySend;
+
+    if (state.server.daemon != IRCServer.Daemon.twitch) return;
+
+    Message m;
+
+    m.event.type = IRCEvent.Type.CHAN;
+    m.event.channel = channelName;
+    m.event.content = content.expandIRCTags;
+    m.event.tags = "@reply-parent-msg-id=" ~ id;
+    m.caller = caller;
+
+    if (quiet) m.properties |= Message.Property.quiet;
+    if (background) m.properties |= Message.Property.background;
+    if (priority) m.properties |= Message.Property.priority;
+
+    import std.algorithm.searching : canFind;
+
+    if (state.bot.homeChannels.canFind(channelName))
+    {
+        // We're in a home channel
+        m.properties |= Message.Property.fast;
+    }
+    /*else if (auto channel = channelName in state.channels)
+    {
+        if ((*channel).ops.canFind(state.client.nickname))
+        {
+            m.properties |= Message.Property.fast;
+        }
+    }*/
+
+    state.mainThread.send(m);
+}
+
+
 // query
 /++
     Sends a private query message to a user.
