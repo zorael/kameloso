bbbd1e45193763aa1669f052f315830c60fea09a
JR <zorael@gmail.com>
2023-02-01 14:39:53 +0100

    v3.8.0
    This release saw some wins in lowering compilation memory required,
    which is now down from around **3100** Mb to **2000** Mb for a Twitch
    build (Linux, x86_64). Some gains were had by consolidating template
    instantiations and being smarter about module reflection, while others
    were from removing or versioning out some fringe functionality. Notably,
    some error messages will be less informative in non-`dev` builds. After
    these changes **gdc** also now seems more likely to successfully link.
    
    The Counter plugin was refactored to use a `Counter` struct instead of a
    naked `long` for counts. This allows us to introduce custom format
    patterns, with which users can now set how counter
    queries/increments/decrements/assignments should be phrased. The command
    is `!counter format`.
    
    Added support for third-party BetterTTV, FrankerFaceZ and 7tv Twitch
    emotes. This makes `!ecount` much more relevant. Channel-specific emotes
    are imported upon joining a channel, and global channel-independent ones
    on program startup, both irrespective of `ecount` setting. Only applies
    to home channels (unless version `TwitchCustomEmotesEverywhere` is set,
    which is left as a debugging feature).
    
    At the time of writing `http://bash.org` has been down for several
    weeks. The Bash plugin remains enabled by default for now, but if the
    site doesn't return we might as well version the plugin out and
    eventually delete it outright.
    
    Other notable changes:
    
    * Quotes were broken on Twitch and should now be fixed
    * `dialect` dependency version bumped to v1.5.0
    * The compatibility alias `--set twitchbot.*` (to `--set twitch.*`) was
      finally removed
    * Twitch `USERSTATE` should no longer spam warnings on all outgoing
      messages
    * The Time plugin was slightly refactored but should behave the same as
      before
    * Refactored many plugins' sending to server into nested functions
    * Added a `messaging` function `reply` that replies to Twitch channel
      messages, but so far it's not being used

--- source/kameloso/semver.d
+++ source/kameloso/semver.d
@@ -11,8 +11,8 @@ module kameloso.semver;
 enum KamelosoSemVer
 {
     majorVersion = 3,  /// SemVer major version of the program.
-    minorVersion = 7,  /// SemVer minor version of the program.
-    patchVersion = 1,  /// SemVer patch version of the program.
+    minorVersion = 8,  /// SemVer minor version of the program.
+    patchVersion = 0,  /// SemVer patch version of the program.
 }
 
 
