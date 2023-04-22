3000fb233b842b742d3999d8a65ffea169cb2508
JR <zorael@gmail.com>
2023-04-22 14:48:42 +0200

    printer: Add missing COM0 and LPT0 log filename escapes

--- source/kameloso/plugins/printer/logging.d
+++ source/kameloso/plugins/printer/logging.d
@@ -704,9 +704,9 @@ auto escapedPath(/*const*/ string path)
         enum replacementCharacter = '~';
         enum alternateReplacementCharacter = ')';
 
-        if (path.toUpper.among!("CON", "PRN", "AUX", "NUL", "COM1", "COM2",
-            "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9", "LPT1",
-            "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"))
+        if (path.toUpper.among!("CON", "PRN", "AUX", "NUL",
+            "COM0", "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+            "LPT0", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9"))
         {
             path ~= replacementCharacter;
         }
