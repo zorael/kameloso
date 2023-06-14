56d91583450fc183b7e03617e86248cf4c5b6478
JR <zorael@gmail.com>
2023-06-15 01:26:34 +0200

    Add Admin bus message verb "reexec"

--- source/kameloso/plugins/admin/base.d
+++ source/kameloso/plugins/admin/base.d
@@ -1412,6 +1412,14 @@ void onBusMessage(
             return;
     }
 
+    version(Posix)
+    {
+        case "reexec":
+            import kameloso.thread : ThreadMessage, boxed;
+            import std.concurrency : prioritySend;
+            return plugin.state.mainThread.prioritySend(ThreadMessage.reconnect(string.init, boxed(true)));
+    }
+
     case "set":
         import kameloso.constants : BufferSize;
         import kameloso.thread : CarryingFiber;
