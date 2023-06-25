5593c7f019446a8a352806f71c28ba34878b0525
JR <zorael@gmail.com>
2023-06-25 19:25:18 +0200

    Rein in the garbage collector incPoolSize a bit

--- source/kameloso/main.d
+++ source/kameloso/main.d
@@ -70,7 +70,7 @@ enum gcOptions = ()
     }
 
     // Tweak these numbers as we see fit
-    sink.put("initReserve:8 minPoolSize:8 incPoolSize:16");
+    sink.put("initReserve:8 minPoolSize:8"); // incPoolSize:16
 
     return sink.data;
 }().idup;
