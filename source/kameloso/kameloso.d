c9b1cfcaf0b07319485ff25daad4aa9d9ee91f1e
JR <zorael@gmail.com>
2024-01-06 11:14:13 +0100

    Avoid a superfluous AA.keys() call

--- source/kameloso/kameloso.d
+++ source/kameloso/kameloso.d
@@ -855,8 +855,10 @@ public:
             }
         }
 
-        return channels.keys.length ?
-            channels.keys :
+        auto keys = channels.keys;
+
+        return keys.length ?
+            keys :
             [ cast(string)MagicStrings.emptyArrayMarker ];
     }
 
