0248eee9a32a4064005ad8fbb6315e11dc5877f4
JR <zorael@gmail.com>
2024-01-09 19:20:13 +0100

    Add kameloso.common.getHTTPResponseCodeText

--- source/kameloso/common.d
+++ source/kameloso/common.d
@@ -658,3 +658,150 @@ enum HTTPVerb
      +/
     unsupported,
 }
+
+
+// getHTTPResponseCodeText
+/++
+    Returns the text associated with an HTTP response code.
+
+    Params:
+        code = HTTP response code.
+
+    Returns:
+        The text associated with the HTTP response code, or "(Unknown HTTP
+        response)" if the code is not recognized.
+ +/
+auto getHTTPResponseCodeText(const uint code)
+{
+    switch (code)
+    {
+    case 0:
+        return "(unset)";
+
+    case 1:
+    ..
+    case 5:
+        return "SSL library error";
+
+    case 100: return "Continue";
+    case 101: return "Switching Protocols";
+    case 102: return "Processing";
+    case 103: return "Early Hints";
+
+    case 200: return "OK";
+    case 201: return "Created";
+    case 202: return "Accepted";
+    case 203: return "Non-Authoritative Information";
+    case 204: return "No Content";
+    case 205: return "Reset Content";
+    case 206: return "Partial Content";
+    case 207: return "Multi-Status";
+    case 208: return "Already Reported";
+    case 226: return "IM Used";
+
+    case 300: return "Multiple Choices";
+    case 301: return "Moved Permanently";
+    case 302: return "Found";
+    case 303: return "See Other";
+    case 304: return "Not Modified";
+    case 305: return "Use Proxy";
+    case 306: return "Switch Proxy";
+    case 307: return "Temporary Redirect";
+    case 308: return "Permanent Redirect";
+
+    case 400: return "Bad Request";
+    case 401: return "Unauthorized";
+    case 402: return "Payment Required";
+    case 403: return "Forbidden";
+    case 404: return "Not Found";
+    case 405: return "Method Not Allowed";
+    case 406: return "Not Acceptable";
+    case 407: return "Proxy Authentication Required";
+    case 408: return "Request Timeout";
+    case 409: return "Conflict";
+    case 410: return "Gone";
+    case 411: return "Length Required";
+    case 412: return "Precondition Failed";
+    case 413: return "Payload Too Large";
+    case 414: return "URI Too Long";
+    case 415: return "Unsupported Media Type";
+    case 416: return "Range Not Satisfiable";
+    case 417: return "Expectation Failed";
+    case 418: return "I'm a teapot";
+    case 421: return "Misdirected Request";
+    case 422: return "Unprocessable Entity";
+    case 423: return "Locked";
+    case 424: return "Failed Dependency";
+    case 425: return "Too Early";
+    case 426: return "Upgrade Required";
+    case 428: return "Precondition Required";
+    case 429: return "Too Many Requests";
+    case 431: return "Request Header Fields Too Large";
+    case 451: return "Unavailable For Legal Reasons";
+
+    case 500: return "Internal Server Error";
+    case 501: return "Not Implemented";
+    case 502: return "Bad Gateway";
+    case 503: return "Service Unavailable";
+    case 504: return "Gateway Timeout";
+    case 505: return "HTTP Version Not Supported";
+    case 506: return "Variant Also Negotiates";
+    case 507: return "Insufficient Storage";
+    case 508: return "Loop Detected";
+    case 510: return "Not Extended";
+    case 511: return "Network Authentication Required";
+
+    case 218: return "This is fine";
+    case 419: return "Page Expired";
+    //case 420: return "Method Failure";
+    case 420: return "Enhance your calm";
+    //case 430: return "Request Header Fields Too Large";
+    case 430: return "Shopify Security Rejection";
+    case 450: return "Blocked by Windows Parental Controls";
+    case 498: return "Invalid Token";
+    case 499: return "Token Required";
+    case 509: return "Bandwidth Limit Exceeded";
+    case 529: return "Site is overloaded";
+    //case 530: return "Site is frozen";
+    case 530: return "Origin DNS Error";
+    case 540: return "Temporarily Disabled";
+    case 598: return "Network Rea Timeout Error";
+    case 599: return "Network Connect Timeout Error";
+    case 783: return "Unexpected Token";
+
+    case 440: return "Login Timeout";
+    case 449: return "Retry With";
+    //case 451: return "Redirect";
+
+    case 444: return "No Response";
+    case 494: return "Request Header Too Large";
+    case 495: return "SSL Certificate Error";
+    case 496: return "SSL Certificate Required";
+    case 497: return "HTTP Request Sent to HTTPS Port";
+    //case 499: return "Client Closed Request";
+
+    case 520: return "Web Server Returned an Unknown Error";
+    case 521: return "Web Server Is Down";
+    case 522: return "Connection Timed Out";
+    case 523: return "Origin Is Unreachable";
+    case 524: return "A Timeout Occurred";
+    case 525: return "SSL Handshake Failed";
+    case 526: return "Invalid SSL Certificate";
+    case 527: return "Railgun Error";
+    //case 530: return "(no text)";
+
+    case 460: return "Client Closed Connection";
+    case 463: return "Too Many IP Addresses";
+    case 464: return "Incompatible Protocol";
+
+    case 110: return "Response is Stale";
+    case 111: return "Revalidation Failed";
+    case 112: return "Disconnected Operation";
+    case 113: return "Heuristic Expiration";
+    case 199: return "Miscellaneous Warning";
+    case 214: return "Transformation Applied";
+    case 299: return "Miscellaneous Persistent Warning";
+
+    default: return "(Unknown HTTP response)";
+    }
+}
