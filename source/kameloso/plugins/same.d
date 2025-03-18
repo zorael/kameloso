031284843823e8e35778a09a9780f4c6eec19334
JR <zorael@gmail.com>
2025-03-18 13:58:30 +0100

    Expand the seen.d example plugin

--- source/kameloso/plugins/same.d
+++ source/kameloso/plugins/same.d
@@ -34,12 +34,19 @@ private:
 
 import kameloso.plugins;
 import kameloso.messaging;
+import kameloso.thread;
 import dialect.defs;
+import core.time;
+import core.thread;
 
 
 // SameSettings
 /++
-    Settings for the Same plugin, to toggle it on or off.
+    Settings for the Same plugin. These are automatically read from and written
+    to disk by other parts of the program as long as it is annotated `@Settings`.
+
+    Use it to store smaller persistent settings for the plugin. For larger things,
+    consider using something like JSON files.
  +/
 @Settings struct SameSettings
 {
@@ -47,6 +54,16 @@ import dialect.defs;
         Whether or not the Same plugin should react to events at all.
      +/
     @Enabler bool enabled = true;
+
+    /++
+        Example setting.
+     +/
+    string someSetting;
+
+    /++
+        Another example setting.
+     +/
+    int someOtherSetting;
 }
 
 
@@ -59,7 +76,8 @@ import dialect.defs;
 
     Only literal matching is made, so matches are case-sensitive and may not be
     trailed by other text. Only messages whose contents are literally the characters
-    "same" are matched.
+    "same" are matched. This can naturally be improved upon, but as a toy example
+    it is kept simple.
  +/
 version(MatchByStringComparison)
 @(IRCEventHandler()
@@ -107,6 +125,136 @@ void onRegexMessageSame(SamePlugin plugin, const IRCEvent event)
 }
 
 
+// setup
+/++
+    Sets up the Same plugin. Think of it as a constructor.
+
+    This is called before connection is established.
+ +/
+void setup(SamePlugin plugin)
+in (Fiber.getThis(), "Tried to setup the Same plugin from outside a fiber")
+{
+    import std.random : uniform;
+    plugin.settings.someOtherSetting = uniform(1, 100);
+}
+
+
+// initResources
+/++
+    Initialises the Same plugin's resources.
+
+    This is where you'd read JSON files, reserve memory, etc. In the case of
+    JSON files, one thing commonly done is to read them in and immediately write
+    them out, to ensure they're valid JSON and to normalise formatting.
+
+    This is called before connection is established but after [setup].
+ +/
+void initResources(SamePlugin plugin)
+{
+    // Allocate resources
+    // Read JSON from disk, save JSON to disk
+}
+
+
+// initialise
+/++
+    Initialises the Same plugin. Do whatever you want here.
+
+    This is called after connection is established.
+ +/
+void initialise(SamePlugin plugin)
+in (Fiber.getThis(), "Tried to initialise the Same plugin from outside a fiber")
+{
+    plugin.settings.someSetting = ((plugin.settings.someOtherSetting % 2) == 0) ?
+        "same" :
+        "so i starve myself for energy";
+
+    plugin.meaningOfLife = 42;
+}
+
+
+// postprocess
+/++
+    Postprocesses events.
+
+    This is called after an [dialect.defs.IRCEvent|IRCEvent] has been parsed but
+    before it is passed to plugins for handling. It allows for modifying the event
+    before plugins have a chance to see it.
+
+    Should return `true` if changes were made such that the event loop should
+    check for messages; `false` otherwise.
+ +/
+auto postprocess(SamePlugin plugin, ref IRCEvent event)
+{
+    // Modify the event in some way, it is passed by ref
+    return false;
+}
+
+
+// onBusMessage
+/++
+    Catches bus messages.
+
+    These are messages sent between plugins. The value of `header` should be
+    an identifier for which plugin the message is intended, and `content` is
+    the message itself.
+ +/
+void onBusMessage(SamePlugin plugin, const string header, /*shared*/ Sendable content)
+{
+    if (header != "same") return;
+
+    // Do something with the message
+}
+
+
+// tick
+/++
+    Called on each iteration of the main loop.
+
+    This can often instead be implemented as a repeating fiber, but the option
+    to use this approach is there.
+
+    Should return `true` if the event loop should check for messages; `false` otherwise.
+ +/
+auto tick(SamePlugin plugin, const Duration duration)
+{
+    // Do something
+    return false;
+}
+
+
+// reload
+/++
+    Reloads the Same plugin.
+
+    What this does is highly plugin-specific, but generally reloads JSON files
+    from disk.
+ +/
+void reload(SamePlugin plugin)
+in (Fiber.getThis(), "Tried to reload the Same plugin from outside a fiber")
+{
+    import std.random : uniform;
+
+    assert(plugin.settings.someOtherSetting > 0);  // from setup
+    plugin.settings.someOtherSetting = uniform(1, 100);
+}
+
+
+// teardown
+/++
+    Tears down the Same plugin. Think of it as a destructor.
+
+    This is called when the connection to the server has been lost and the
+    program is either exiting or is getting ready to reconnect
+    (which would call [setup]).
+ +/
+void teardown(SamePlugin plugin)
+in (Fiber.getThis(), "Tried to teardown the Same plugin from outside a fiber")
+{
+    assert(plugin.meaningOfLife == 42);
+}
+
+
 mixin PluginRegistration!SamePlugin;
 
 public:
@@ -122,6 +270,17 @@ final class SamePlugin : IRCPlugin
         All Same plugin settings gathered.
      +/
     SameSettings settings;
+
+    /++
+        Non-settings transient state goes here and ideally not at the module-level.
+     +/
+    int meaningOfLife;
+
+    /++
+        Resource files may be declared here. The filename string will be expanded
+        to the full path of the file as long as it is annotated `@Resource`.
+     +/
+    @Resource string sameFile = "same.json";
 
     mixin IRCPluginImpl;
 }
